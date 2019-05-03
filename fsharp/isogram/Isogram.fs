e12a1f06065d27e3d363f64d9d83df9b416d91bf
slaymance <shane.laymance@gmail.com>
2019-05-02 17:44:57 -0700

    Refactor isogram

--- fsharp/isogram/Isogram.fs
+++ fsharp/isogram/Isogram.fs
@@ -1,7 +1,5 @@
 ﻿module Isogram
 
 let isIsogram (str: string) =
-  let letters = str.ToLower() |> Seq.filter (fun c -> System.Char.IsLetter (c))
-  letters
-    |> Seq.distinct
-    |> Seq.length = Seq.length letters
+  let letters = str.ToLower() |> Seq.filter System.Char.IsLetter
+  Seq.length letters = Seq.length (Seq.distinct letters)
