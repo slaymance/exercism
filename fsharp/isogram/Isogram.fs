2412f17e49e42a0796874e3e22fe0825097be32c
slaymance <shane.laymance@gmail.com>
2019-05-02 17:41:39 -0700

    Solve isogram

---
+++ fsharp/isogram/Isogram.fs
@@ -0,0 +1,7 @@
+﻿module Isogram
+
+let isIsogram (str: string) =
+  let letters = str.ToLower() |> Seq.filter (fun c -> System.Char.IsLetter (c))
+  letters
+    |> Seq.distinct
+    |> Seq.length = Seq.length letters
