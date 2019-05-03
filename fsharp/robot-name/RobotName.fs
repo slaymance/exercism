cc17208afa5a5d8d92dc169ae84fcaf8ef1a0e32
slaymance <shane.laymance@gmail.com>
2019-05-02 17:11:01 -0700

    Solve robot-name

--- fsharp/robot-name/RobotName.fs
+++ fsharp/robot-name/RobotName.fs
@@ -5,26 +5,20 @@ let robotNames = Set.empty
 type Robot = {robotName: string}
 
 let rec generateName() =
-    let rand = System.Random()
-    let letterSeq = {'A' .. 'Z'}
-    let numberSeq = {'0' .. '9'}
-    let letterLength = Seq.length(letterSeq)
-    let numberLength = Seq.length(numberSeq)
-    let letter1 = Seq.item (rand.Next(letterLength)) letterSeq
-    let letter2 = Seq.item (rand.Next(letterLength)) letterSeq
-    let number1 = Seq.item (rand.Next(numberLength)) numberSeq
-    let number2 = Seq.item (rand.Next(numberLength)) numberSeq
-    let number3 = Seq.item (rand.Next(numberLength)) numberSeq
-    let robotName = String.concat "" <| List.map string [letter1; letter2; number1; number2; number3;]
-    let origCount = robotNames.Count
-    let newCount = robotNames.Add(robotName).Count
-    if origCount <> newCount then robotName
-    else generateName ()
+  let rand = System.Random().Next
+  let letterSeq = {'A' .. 'Z'}
+  let robotName =
+    sprintf "%c%c%03i"
+      (letterSeq |> Seq.item (rand(26)))
+      (letterSeq |> Seq.item (rand(26)))
+      (rand(999))
+  if robotNames.Count <> robotNames.Add(robotName).Count then robotName
+  else generateName()
 
 let mkRobot(): Robot = { robotName = generateName() }
 
+let name(robot: Robot) = robot.robotName
 
-let name (robot: Robot) = robot.robotName
-
-
-let reset (robot: Robot) = { robot with robotName = generateName() }
+let reset(robot: Robot) =
+  robotNames.Remove(robot.robotName) |> ignore
+  { robot with robotName = generateName() }
