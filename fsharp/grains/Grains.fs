f311a0cf7d534297b5cf207edf1661cc7bc40c62
slaymance <shane.laymance@gmail.com>
2019-05-04 15:32:46 -0700

    Solve grains

---
+++ fsharp/grains/Grains.fs
@@ -0,0 +1,12 @@
+﻿module Grains
+
+let exponentiate (n: int): uint64 =
+  pown 2UL (n - 1)
+
+let square (n: int): Result<uint64,string> =
+  match n with
+  | _ when n < 1 || n > 64 -> Error "square must be between 1 and 64"
+  | _ -> exponentiate n |> Ok
+
+let total: Result<uint64,string> =
+  {1 .. 64} |> Seq.sumBy exponentiate |> Ok
