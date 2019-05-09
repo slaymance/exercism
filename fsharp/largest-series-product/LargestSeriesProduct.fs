acbfa7ed8e197fe59642bd8115af6b8c16988966
slaymance <shane.laymance@gmail.com>
2019-05-09 08:47:41 -0700

    Solve largest-series-product

---
+++ fsharp/largest-series-product/LargestSeriesProduct.fs
@@ -0,0 +1,15 @@
+﻿module LargestSeriesProduct
+
+let largestProduct (input: string) seriesLength : int option =
+  match input with
+  | _ when input |> Seq.length < seriesLength -> None
+  | _ when input |> Seq.isEmpty && seriesLength > 0 -> None
+  | _ when input |> Seq.exists (System.Char.IsDigit >> not) -> None
+  | _ when seriesLength < 0 -> None
+  | _ when seriesLength = 0 -> Some 1
+  | _ -> input
+      |> Seq.map (System.Char.GetNumericValue >> int)
+      |> Seq.windowed seriesLength
+      |> Seq.map (Seq.fold (*) 1)
+      |> Seq.max
+      |> Some
