727f0903140f7157bfd18046c013911cd3b17624
slaymance <shane.laymance@gmail.com>
2019-05-09 09:49:34 -0700

    Solve nth-prime

---
+++ fsharp/nth-prime/NthPrime.fs
@@ -0,0 +1,13 @@
+﻿module NthPrime
+
+let prime nth : int option =
+  match nth with
+  | 0 -> None
+  | _ ->
+    Seq.initInfinite id |> Seq.filter (fun num ->
+      match num with
+      | 0 -> false
+      | 1 -> false
+      | 2 -> true
+      | _ -> (Seq.exists (fun x -> num % x = 0) {2 .. num - 1}) |> not
+    ) |> Seq.item (nth - 1) |> Some
