536874e92055581371a053aef8e5e35655f48923
slaymance <shane.laymance@gmail.com>
2019-05-13 00:26:21 -0700

    Solve nth-prime

--- fsharp/nth-prime/NthPrime.fs
+++ fsharp/nth-prime/NthPrime.fs
@@ -1,13 +1,22 @@
 ﻿module NthPrime
 
+let isPrime num =
+  Seq.exists (fun x -> num % x = 0) {2 .. float num |> sqrt |> int} |> not
+
+let rec possiblePrimeNumbers num = seq {
+  yield num - 1
+  yield num + 1
+  yield! possiblePrimeNumbers (num + 6)
+}
+
+let primeNumbers = seq {
+  yield 2
+  yield 3
+  yield! Seq.filter isPrime (possiblePrimeNumbers 6)
+}
+
 let prime nth : int option =
   match nth with
   | 0 -> None
   | _ ->
-    Seq.initInfinite id |> Seq.filter (fun num ->
-      match num with
-      | 0 -> false
-      | 1 -> false
-      | 2 -> true
-      | _ -> (Seq.exists (fun x -> num % x = 0) {2 .. num - 1}) |> not
-    ) |> Seq.item (nth - 1) |> Some
+    primeNumbers |> Seq.item (nth - 1) |> Some
