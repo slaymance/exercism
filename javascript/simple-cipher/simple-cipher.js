0b1cdd66bcf55eea576d8135918e16d454f3032a
Shane Laymance <shane.laymance@gmail.com>
2020-10-09 13:22:03 -0700

    Create random key method

--- javascript/simple-cipher/simple-cipher.js
+++ javascript/simple-cipher/simple-cipher.js
@@ -10,21 +10,34 @@ export class Cipher {
   #key
   #keyShift
 
-  constructor(key = 'aaaaaaaaaa') {
+  /**
+   * #keyShift is an array of the numerical values which represent shifts based on the key
+   * e.g. a = 0, b = 1, c = 2, etc.
+   */
+  constructor(key = Cipher.generateRandomKey()) {
     this.#key = key;
     this.#keyShift = [...key].map(char => char.toLowerCase().charCodeAt(0) - ASCII_START);
   }
 
+  /**
+   * Calculates each character's shift based on the key and interprets its ascii value
+   * Wraps to first character of key if text is longer than key
+   */
   encode([...chars]) {
     return chars.map((char, i) => String.fromCharCode(
       ((char.charCodeAt(0) - ASCII_START) + (this.#keyShift[i % this.#keyShift.length])) % ASCII_LENGTH + ASCII_START)
     ).join('');
   }
 
+  // Similar to encode, but subtracts the shift for each character based on the key
   decode([...chars]) {
     return chars.map((char, i) => String.fromCharCode(
       ((char.charCodeAt(0) - ASCII_START) - (this.#keyShift[i % this.#keyShift.length]) + ASCII_LENGTH) % ASCII_LENGTH + ASCII_START)
     ).join('');
+  }
+
+  static generateRandomKey() {
+    return [...Array(100)].map(() => String.fromCharCode(Math.floor(Math.random() * ASCII_LENGTH) + ASCII_START)).join('');
   }
 
   get key() {
