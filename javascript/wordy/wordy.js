f76f42af938ba1486a6c4d19b23694cd75000504
Shane Laymance <shane.laymance@gmail.com>
2020-10-10 16:20:53 -0700

    Clean up code with helper functions

--- javascript/wordy/wordy.js
+++ javascript/wordy/wordy.js
@@ -1,8 +1,4 @@
-//
-// This is only a SKELETON file for the 'Wordy' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
+// Mapping of string operators to their javascript mathematical equivalents
 const OPERATOR_MAP = {
   '**': (num1, num2) => num1 ** num2,
   '*': (num1, num2) => num1 * num2,
@@ -10,6 +6,11 @@ const OPERATOR_MAP = {
   '+': (num1, num2) => num1 + num2,
   '-': (num1, num2) => num1 - num2
 };
+
+// Helper functions for verifying words in the supplied question
+const isNumber = x => !isNaN(x);
+const isEven = int => int % 2 === 0;
+const isOperatorString = word => typeof word === 'string' && word.match(/[\+\-\*\/]/);
 
 const produceValidEquation = question => {
   // Parse the question into an array made up of numbers and mathematical operators
@@ -22,28 +23,28 @@ const produceValidEquation = question => {
     .replace(/(st|nd|rd|th)? power/g, '')
     .replace('?', ' ?')
     .split(' ')
-    .map(word => isNaN(word) ? word : Number(word));
+    .map(word => isNumber(word) ? Number(word) : word);
 
   // Make sure the question is in the form of 'What is ...?', numbers, and operators
   if (!parsedQuestion.every((word, i) => {
     if (i === 0) return word === 'What';
     if (i === 1) return word === 'is';
     if (i === parsedQuestion.length - 1) return word === '?';
-    return !isNaN(word) || word.match(/[\+\-\*\/]/);
+    return isNumber(word) || isOperatorString(word);
   })) throw new Error('Unknown operation');
 
   // Check for proper syntax
+  // Any valid expression in this case will have an odd number of symbols
+  // If index is even, then the element should be an operator; if odd, then it should be a number
   const equation = parsedQuestion.slice(2, -1);
   if (
-    equation.length % 2 !== 1 // Any valid expression in this case will have an odd number of symbols
-    || !equation.every((word, i) => i % 2 === 0 ? // If index is odd, then the element should be a number; if even, then it should be an operator
-      !isNaN(word)
-      : typeof word === 'string' && word.match(/[\+\-\*\/]/))) {
-    throw new Error('Syntax error');
-  }
+    isEven(equation.length) || !equation.every(
+      (word, i) => isEven(i) ? isNumber(word) : isOperatorString(word)
+    )
+  ) throw new Error('Syntax error');
 
   return equation;
 };
 
 export const answer = question => produceValidEquation(question)
-  .reduce((acc, cur, i, src) => isNaN(cur) ? OPERATOR_MAP[cur](acc, src[i + 1]) : acc);
+  .reduce((acc, cur, i, src) => isNumber(cur) ? acc : OPERATOR_MAP[cur](acc, src[i + 1]));
