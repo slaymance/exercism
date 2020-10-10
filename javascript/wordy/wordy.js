68a8566be105208a64dc27920ed2f2c7a75f9d21
Shane Laymance <shane.laymance@gmail.com>
2020-10-10 16:36:53 -0700

    Separate functions by concern

--- javascript/wordy/wordy.js
+++ javascript/wordy/wordy.js
@@ -12,20 +12,20 @@ const isNumber = x => !isNaN(x);
 const isEven = int => int % 2 === 0;
 const isOperatorString = word => typeof word === 'string' && word.match(/[\+\-\*\/]/);
 
-const produceValidEquation = question => {
-  // Parse the question into an array made up of numbers and mathematical operators
-  const parsedQuestion = question
-    .replace(/plus/g, '+')
-    .replace(/minus/g, '-')
-    .replace(/multiplied by/g, '*')
-    .replace(/divided by/g, '/')
-    .replace(/raised to the/g, '**')
-    .replace(/(st|nd|rd|th)? power/g, '')
-    .replace('?', ' ?')
-    .split(' ')
-    .map(word => isNumber(word) ? Number(word) : word);
+// Parse the question into an array made up of the question, numbers, and mathematical operators
+const parseQuestion = question => question
+  .replace(/plus/g, '+')
+  .replace(/minus/g, '-')
+  .replace(/multiplied by/g, '*')
+  .replace(/divided by/g, '/')
+  .replace(/raised to the/g, '**')
+  .replace(/(st|nd|rd|th)? power/g, '')
+  .replace('?', ' ?')
+  .split(' ')
+  .map(word => isNumber(word) ? Number(word) : word);
 
-  // Make sure the question is in the form of 'What is ...?', numbers, and operators
+// Make sure the question is in the form of 'What is ...?', numbers, and operators
+const checkForUnknownOperation = parsedQuestion => {
   if (!parsedQuestion.every((word, i) => {
     if (i === 0) return word === 'What';
     if (i === 1) return word === 'is';
@@ -33,10 +33,15 @@ const produceValidEquation = question => {
     return isNumber(word) || isOperatorString(word);
   })) throw new Error('Unknown operation');
 
+  return parsedQuestion;
+};
+
+const produceValidExpression = question => {
+  const expression = checkForUnknownOperation(parseQuestion(question)).slice(2, -1);
+
   // Check for proper syntax
   // Any valid expression in this case will have an odd number of symbols
   // If index is even, then the element should be a number; if odd, then it should be an operator
-  const expression = parsedQuestion.slice(2, -1);
   if (
     isEven(expression.length) || !expression.every(
       (word, i) => isEven(i) ? isNumber(word) : isOperatorString(word)
@@ -46,5 +51,5 @@ const produceValidEquation = question => {
   return expression;
 };
 
-export const answer = question => produceValidEquation(question)
+export const answer = question => produceValidExpression(question)
   .reduce((acc, cur, i, src) => isNumber(cur) ? acc : OPERATOR_MAP[cur](acc, src[i + 1]));
