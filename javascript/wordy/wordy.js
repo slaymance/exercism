475823973f17af702ce62c5481377d9768054e52
Shane Laymance <shane.laymance@gmail.com>
2020-10-10 09:49:04 -0700

    Start javascript/wordy

---
+++ javascript/wordy/wordy.js
@@ -0,0 +1,8 @@
+//
+// This is only a SKELETON file for the 'Wordy' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const answer = () => {
+  throw new Error("Remove this statement and implement this function");
+};
