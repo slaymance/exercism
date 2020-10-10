12e0bc7d7518236b6fad005102c6a64f130bc42c
Shane Laymance <shane.laymance@gmail.com>
2020-10-10 13:07:34 -0700

    Add comments and cleanup regex

--- javascript/wordy/wordy.js
+++ javascript/wordy/wordy.js
@@ -12,6 +12,7 @@ const OPERATOR_MAP = {
 };
 
 const produceValidEquation = question => {
+  // Parse the question into an array made up of numbers and mathematical operators
   const parsedQuestion = question
     .split('?')[0]
     .replace(/plus/g, '+')
@@ -19,22 +20,22 @@ const produceValidEquation = question => {
     .replace(/multiplied by/g, '*')
     .replace(/divided by/g, '/')
     .replace(/raised to the/g, '**')
-    .replace(/st power/g, '')
-    .replace(/rd power/g, '')
-    .replace(/th power/g, '')
+    .replace(/(st|nd|rd|th)? power/g, '')
     .split(' ')
     .map(word => isNaN(word) ? word : Number(word));
 
+  // Make sure the question is in the form of 'What is ...?', numbers, and operators
   if (!parsedQuestion.every((word, i) => {
     if (i === 0) return word === 'What';
     if (i === 1) return word === 'is';
     return !isNaN(word) || word.match(/[\+\-\*\/]/);
   })) throw new Error('Unknown operation');
 
+  // Check for proper syntax
   const equation = parsedQuestion.slice(2);
   if (
-    equation.length % 2 !== 1
-    || !equation.every((word, i) => i % 2 === 0 ?
+    equation.length % 2 !== 1 // Any valid expression in this case will have an odd number of symbols
+    || !equation.every((word, i) => i % 2 === 0 ? // If index is odd, then the element should be a number; if even, then it should be an operator
       !isNaN(word)
       : typeof word === 'string' && word.match(/[\+\-\*\/]/))) {
     throw new Error('Syntax error');
