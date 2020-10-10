6e64eaf26ea93eb0fd3821a87f52fba5a3aec37f
Shane Laymance <shane.laymance@gmail.com>
2020-10-10 15:54:47 -0700

    Checks for question mark

--- javascript/wordy/wordy.js
+++ javascript/wordy/wordy.js
@@ -14,13 +14,13 @@ const OPERATOR_MAP = {
 const produceValidEquation = question => {
   // Parse the question into an array made up of numbers and mathematical operators
   const parsedQuestion = question
-    .split('?')[0]
     .replace(/plus/g, '+')
     .replace(/minus/g, '-')
     .replace(/multiplied by/g, '*')
     .replace(/divided by/g, '/')
     .replace(/raised to the/g, '**')
     .replace(/(st|nd|rd|th)? power/g, '')
+    .replace('?', ' ?')
     .split(' ')
     .map(word => isNaN(word) ? word : Number(word));
 
@@ -28,11 +28,12 @@ const produceValidEquation = question => {
   if (!parsedQuestion.every((word, i) => {
     if (i === 0) return word === 'What';
     if (i === 1) return word === 'is';
+    if (i === parsedQuestion.length - 1) return word === '?';
     return !isNaN(word) || word.match(/[\+\-\*\/]/);
   })) throw new Error('Unknown operation');
 
   // Check for proper syntax
-  const equation = parsedQuestion.slice(2);
+  const equation = parsedQuestion.slice(2, -1);
   if (
     equation.length % 2 !== 1 // Any valid expression in this case will have an odd number of symbols
     || !equation.every((word, i) => i % 2 === 0 ? // If index is odd, then the element should be a number; if even, then it should be an operator
