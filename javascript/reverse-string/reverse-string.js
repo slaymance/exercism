605935a46a02bcb0a4c9cd132c4ba2bf7f81395d
Shane Laymance <shane.laymance@gmail.com>
2020-10-14 14:47:24 -0700

    Complete javascript/reverse-string

---
+++ javascript/reverse-string/reverse-string.js
@@ -0,0 +1,6 @@
+//
+// This is only a SKELETON file for the 'Reverse String' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const reverseString = ([...chars]) => chars.reverse().join('');
---
+++ javascript/reverse-string/reverse-string.spec.js
@@ -0,0 +1,39 @@
+import { reverseString } from './reverse-string';
+
+describe('ReverseString', () => {
+  test('empty string', () => {
+    const expected = '';
+    const actual = reverseString('');
+    expect(actual).toEqual(expected);
+  });
+
+  test('a word', () => {
+    const expected = 'tobor';
+    const actual = reverseString('robot');
+    expect(actual).toEqual(expected);
+  });
+
+  test('a capitalized word', () => {
+    const expected = 'nemaR';
+    const actual = reverseString('Ramen');
+    expect(actual).toEqual(expected);
+  });
+
+  test('a sentence with punctuation', () => {
+    const expected = '!yrgnuh ma I';
+    const actual = reverseString('I am hungry!');
+    expect(actual).toEqual(expected);
+  });
+
+  test('a palindrome', () => {
+    const expected = 'racecar';
+    const actual = reverseString('racecar');
+    expect(actual).toEqual(expected);
+  });
+
+  test('an even-sized word', () => {
+    const expected = 'reward';
+    const actual = reverseString('drawer');
+    expect(actual).toEqual(expected);
+  });
+});
