4eb59459f288b00d619b9f508ddf6eeaf3a12abd
Shane Laymance <shane.laymance@gmail.com>
2020-10-14 15:44:15 -0700

    Complete javascript/collatz-conjecture

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -3,6 +3,11 @@
 // convenience to get you started writing code faster.
 //
 
-export const steps = () => {
-  throw new Error("Remove this statement and implement this function");
+const calculateSteps = (n, stepCount = 0) =>
+  n === 1 ? stepCount : calculateSteps(n & 1 ? 3 * n + 1 : n / 2, ++stepCount);
+
+export const steps = n => {
+  if (n <= 0) throw new Error('Only positive numbers are allowed');
+
+  return calculateSteps(n);
 };
