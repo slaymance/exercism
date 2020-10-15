273c04811531911d461c9c62d1bc44862f9ea497
Shane Laymance <shane.laymance@gmail.com>
2020-10-15 08:53:09 -0700

    Start javascript/triangle

---
+++ javascript/triangle/triangle.js
@@ -0,0 +1,22 @@
+//
+// This is only a SKELETON file for the 'Triangle' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class Triangle {
+  constructor() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  isEquilateral() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  isIsosceles() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  isScalene() {
+    throw new Error("Remove this statement and implement this function");
+  }
+}
