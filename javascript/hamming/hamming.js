12b0b72b4e726f41119566ce09af9bb6f0962ecc
Shane Laymance <shane.laymance@gmail.com>
2020-10-19 09:33:42 -0700

    Complete javascript/hamming

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -3,6 +3,10 @@
 // convenience to get you started writing code faster.
 //
 
-export const compute = () => {
-  throw new Error("Remove this statement and implement this function");
+export const compute = (str1, str2) => {
+  if (!str1.length && str2.length) throw new Error('left strand must not be empty');
+  if (str1.length && !str2.length) throw new Error('right strand must not be empty');
+  if (str1.length !== str2.length) throw new Error('left and right strands must be of equal length');
+
+  return [...str1].reduce((acc, cur, i) => cur === str2[i] ? acc : acc + 1, 0);
 };
