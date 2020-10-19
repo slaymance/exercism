44c3fe1962853c4dc79a0fca2b0ee14a4d8fa3be
Shane Laymance <shane.laymance@gmail.com>
2020-10-18 22:06:11 -0700

    Complete javascript/etl

--- javascript/etl/etl.js
+++ javascript/etl/etl.js
@@ -3,6 +3,46 @@
 // convenience to get you started writing code faster.
 //
 
-export const transform = () => {
-  throw new Error("Remove this statement and implement this function");
-};
+/**
+ * This is taken from Lodash's source code.
+ * https://lodash.com/docs/4.17.15#fromPairs
+ */
+function fromPairs(pairs) {
+  var index = -1,
+    length = pairs == null ? 0 : pairs.length,
+    result = {};
+
+  while (++index < length) {
+    var pair = pairs[index];
+    result[pair[0]] = pair[1];
+  }
+  return result;
+}
+
+/**
+ * I wanted to try my hand at creating an iterator with a generator. It's mostly unnecessary for this problem, but
+ * could be useful if the data weren't being transformed and migrated and being used throughout an application. Plus 
+ * it's just a language feature I've never used before.
+ */
+export const transform = scores => fromPairs([...{
+  ...scores,
+  /**
+   * This is the iterator that yields a pairing of a lower case letter and its score on each iteration.
+   * e.g. ['a', 1]
+   */
+  *[Symbol.iterator]() {
+    const scores = Object.keys(this);
+    const letters = Object.values(this);
+
+    let scoreIndex = -1;
+    let letterIndex = -1;
+
+    while (++scoreIndex < scores.length) {
+      while (++letterIndex < letters[scoreIndex].length) {
+        yield [letters[scoreIndex][letterIndex].toLowerCase(), Number(scores[scoreIndex])]; // This is the pairing
+      }
+      letterIndex = -1;
+    }
+  }
+}]);
+
