561bedbc8787d7135e7faf1ddbc14a6acc9652a9
Shane Laymance <shane.laymance@gmail.com>
2020-10-19 13:34:14 -0700

    Refactor to use Object.fromEntries

--- javascript/etl/etl.js
+++ javascript/etl/etl.js
@@ -7,23 +7,25 @@
  * I wanted to try my hand at creating an iterator with a generator. It's mostly unnecessary for this problem, but
  * could be useful if the data weren't being transformed and migrated and being used throughout an application. Plus 
  * it's just a language feature I've never used before.
+ * 
+ * Object.fromEntries requires Node version >12.0.0.
  */
-export const transform = scores => [...{
+export const transform = scores => Object.fromEntries([...{
   ...scores,
   /**
    * This is the iterator that yields a pairing of a lower case letter and its score on each iteration.
-   * e.g. { 'a': 1 }
+   * e.g. ['a', 1]
    */
   *[Symbol.iterator]() {
     for (let [score, letters] of Object.entries(this)) {
       for (let letter of letters) {
-        yield { [letter.toLowerCase()]: +score }; // This is the pairing
+        yield [letter.toLowerCase(), +score]; // This is the pairing
       }
     }
   }
-}].reduce((acc, cur) => ({ ...acc, ...cur }));
+}]);
 
 /**
  * Here's a one-liner.
  */
-export const transformAlt = scores => Object.entries(scores).reduce((acc, [score, letters]) => ({ ...acc, ...(letters.reduce((acc2, letter) => ({ ...acc2, [letter.toLowerCase()]: +score }), {})) }), {})
+export const transformAlt = scores => Object.fromEntries(Object.entries(scores).reduce((acc, [score, letters]) => [...acc, ...letters.map(letter => [letter.toLowerCase(), +score])], []));
