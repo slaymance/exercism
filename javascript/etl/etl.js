3c014ca20698b06584631d156e4a6e35eb1afba8
Shane Laymance <shane.laymance@gmail.com>
2020-10-19 02:29:30 -0700

    Clean up iterator function

--- javascript/etl/etl.js
+++ javascript/etl/etl.js
@@ -4,49 +4,26 @@
 //
 
 /**
- * This is taken from Lodash's source code.
- * https://lodash.com/docs/4.17.15#fromPairs
- */
-function fromPairs(pairs) {
-  var index = -1,
-    length = pairs == null ? 0 : pairs.length,
-    result = {};
-
-  while (++index < length) {
-    var pair = pairs[index];
-    result[pair[0]] = pair[1];
-  }
-  return result;
-}
-
-/**
  * I wanted to try my hand at creating an iterator with a generator. It's mostly unnecessary for this problem, but
  * could be useful if the data weren't being transformed and migrated and being used throughout an application. Plus 
  * it's just a language feature I've never used before.
  */
-export const transform = scores => fromPairs([...{
+export const transform = scores => [...{
   ...scores,
   /**
    * This is the iterator that yields a pairing of a lower case letter and its score on each iteration.
-   * e.g. ['a', 1]
+   * e.g. { 'a': 1 }
    */
   *[Symbol.iterator]() {
-    const scores = Object.keys(this);
-    const letters = Object.values(this);
-
-    let scoreIndex = -1;
-    let letterIndex = -1;
-
-    while (++scoreIndex < scores.length) {
-      while (++letterIndex < letters[scoreIndex].length) {
-        yield [letters[scoreIndex][letterIndex].toLowerCase(), Number(scores[scoreIndex])]; // This is the pairing
+    for (let [score, letters] of Object.entries(this)) {
+      for (let letter of letters) {
+        yield { [letter.toLowerCase()]: +score }; // This is the pairing
       }
-      letterIndex = -1;
     }
   }
-}]);
+}].reduce((acc, cur) => ({ ...acc, ...cur }));
 
 /**
  * Here's a one-liner.
  */
-export const transformAlt = scores => Object.entries(scores).reduce((acc, [score, letters]) => ({ ...acc, ...(letters.reduce((acc2, letter) => ({ ...acc2, [letter.toLowerCase()]: Number(score) }), {})) }), {})
+export const transformAlt = scores => Object.entries(scores).reduce((acc, [score, letters]) => ({ ...acc, ...(letters.reduce((acc2, letter) => ({ ...acc2, [letter.toLowerCase()]: +score }), {})) }), {})
