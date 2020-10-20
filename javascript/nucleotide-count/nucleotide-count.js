5f3d3b4aa01284c34b0bd37be7621b0cc14de74e
Shane Laymance <shane.laymance@gmail.com>
2020-10-20 10:49:00 -0700

    Complete javascript/nucleotide-count

--- javascript/nucleotide-count/nucleotide-count.js
+++ javascript/nucleotide-count/nucleotide-count.js
@@ -3,8 +3,17 @@
 // convenience to get you started writing code faster.
 //
 
-export class NucleotideCounts {
-  static parse() {
-    throw new Error("Remove this statement and implement this function");
-  }
-}
+const NUCLEOTIDE_INDECES = {
+  'A': 0,
+  'C': 1,
+  'G': 2,
+  'T': 3
+};
+
+/**
+ * I refactored the problem and its tests to just use a function instead of a class.
+ */
+export const parse = ([...strand]) => strand.reduce((counts, nucleotide) => {
+  if (NUCLEOTIDE_INDECES[nucleotide] === undefined) throw new Error('Invalid nucleotide in strand');
+  return ++counts[NUCLEOTIDE_INDECES[nucleotide]] && counts;
+}, [0, 0, 0, 0]).join(' ');
