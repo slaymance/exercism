3b9f18e6a83371aa3cb604288501aed7ed1e8c46
Shane Laymance <shane.laymance@gmail.com>
2020-10-23 17:58:31 -0700

    Change in regex

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -7,7 +7,7 @@
  * Little shakey on the regex but I'll try to explain:
  * \w+ gets all the word characters together (an easy way to deal with all the words that aren't contractions)
  * \' looks for apostrophes that follow word characters (so eliminates beginning single quotes)
- * [a-z]+ makes sure the apostrophe has any number of word characters that follow it (so this technically works for
+ * \w+ makes sure the apostrophe has any number of word characters that follow it (so this technically works for
  * words like y'all)
  * ? says the stuff in parentheses is optional as not all words are contractions
  * g matches the whole string
@@ -16,5 +16,5 @@
  */
 export const countWords = phrase => phrase
   .toLowerCase()
-  .match(/\w+(\'[a-z]+)?/g)
+  .match(/\w+(\'\w+)?/g)
   .reduce((counts, word) => ({ ...counts, [word]: ~~counts[word] + 1 }), {});
