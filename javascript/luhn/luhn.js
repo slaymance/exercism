7009878af220f77abd011e35f869738501762aa4
Shane Laymance <shane.laymance@gmail.com>
2020-10-26 12:08:56 -0700

    Break out functions for better clarity

--- javascript/luhn/luhn.js
+++ javascript/luhn/luhn.js
@@ -3,11 +3,15 @@
 // convenience to get you started writing code faster.
 //
 
+// Some helper functions for better clarity
 const isNumber = n => typeof n === 'number' && !isNaN(n) && isFinite(n);
+const isDivisibleByTen = n => !(n % 10);
+const isOdd = n => !!(n & 1);
+const luhnReduction = (sum, num, i, { length }) => sum + num + (!isOdd(length - i) && num - (num >= 5 && 9));
 
 export const valid = string => {
   const nums = [...string.replace(/\s/g, '')].map(Number);
   return nums.length > 1 &&
     nums.every(isNumber) &&
-    !(nums.reduceRight((sum, num, i, src) => sum + num + (!(src.length - i & 1) && num - (num >= 5 && 9))) % 10);
+    isDivisibleByTen(nums.reduceRight(luhnReduction));
 };
