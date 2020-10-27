0679f8a45a70fb830b611eabb92775440a516c4c
Shane Laymance <shane.laymance@gmail.com>
2020-10-26 23:21:44 -0700

    Some cryptic refactoring

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -3,11 +3,10 @@
 // convenience to get you started writing code faster.
 //
 
-const calculateSteps = (n, stepCount = 0) =>
-  n === 1 ? stepCount : calculateSteps(n & 1 ? 3 * n + 1 : n / 2, stepCount + 1);
+const calculateSteps = n =>
+  +(n !== 1) && 1 + calculateSteps(n & 1 ? 3 * n + 1 : n / 2);
 
 export const steps = n => {
   if (n <= 0) throw new Error('Only positive numbers are allowed');
-
   return calculateSteps(n);
 };
