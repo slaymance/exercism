dac44dde78d5960824318fea52ca4e39b05db03c
Shane Laymance <shane.laymance@gmail.com>
2020-10-28 10:27:20 -0700

    Complete javascript/grains

---
+++ javascript/grains/grains.js
@@ -0,0 +1,14 @@
+//
+// This is only a SKELETON file for the 'Grains' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+const CHESSBOARD_SIZE = 64;
+
+export const square = num => {
+  if (num < 1 || num > CHESSBOARD_SIZE) throw new Error(`square must be between 1 and ${CHESSBOARD_SIZE}`);
+  return 1n << BigInt(num - 1);
+};
+
+export const total = () => [...Array(CHESSBOARD_SIZE + 1).keys()]
+  .reduce((sum, squareNum) => BigInt(sum) + square(squareNum));
---
+++ javascript/grains/grains.spec.js
@@ -0,0 +1,84 @@
+/**
+ * In JavaScript, integers beyond +/- 9007199254740991 (available as the
+ * Number.MIN_SAFE_INTEGER and NUMBER.MAX_SAFE_INTEGER constants) cannot be
+ * accurately represented by the default "Number" type. To see this in action,
+ * console.log() out the expected number of grains on square #64:
+ *
+ * console.log(9223372036854775808);
+ * // =>       9223372036854776000
+ * //                         ^^^^
+ *
+ * This is because, in JavaScript, "Number" type integers are actually
+ * represented internally as 64-bit floating point numbers. If you want to learn
+ * more, see:
+ *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/MAX_SAFE_INTEGER
+ * http://stackoverflow.com/questions/307179/what-is-javascripts-highest-integer-value-that-a-number-can-go-to-without-losin
+ *
+ * Historically, this limitation could only be overcome by use third-party
+ * implementations of a "bit integer" type like "BigInteger.js"
+ * (https://github.com/peterolson/BigInteger.js).
+ *
+ * However, a new JavaScript arbitrary precision integer type named "BigInt",
+ * which you should use to solve this exercise, is in the late stages of
+ * ECMAScript standardisation and already available in most major browsers and
+ * Node.js >= 10.4.0. For more details see:
+ *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt
+ */
+
+import { square, total } from './grains';
+
+describe('Grains', () => {
+  describe('Returns The Number Of Grains On The Square', () => {
+    test('1', () => {
+      expect(square(1).toString()).toEqual('1');
+    });
+
+    test('2', () => {
+      expect(square(2).toString()).toEqual('2');
+    });
+
+    test('3', () => {
+      expect(square(3).toString()).toEqual('4');
+    });
+
+    test('4', () => {
+      expect(square(4).toString()).toEqual('8');
+    });
+
+    test('16', () => {
+      expect(square(16).toString()).toEqual('32768');
+    });
+
+    test('32', () => {
+      expect(square(32).toString()).toEqual('2147483648');
+    });
+
+    test('64', () => {
+      expect(square(64).toString()).toEqual('9223372036854775808');
+    });
+
+    test('square 0 raises an exception', () => {
+      expect(() => square(0)).toThrow(
+        new Error('square must be between 1 and 64')
+      );
+    });
+
+    test('negative square raises an exception', () => {
+      expect(() => square(-1)).toThrow(
+        new Error('square must be between 1 and 64')
+      );
+    });
+
+    test('square greater than 64 raises an exception', () => {
+      expect(() => square(65)).toThrow(
+        new Error('square must be between 1 and 64')
+      );
+    });
+  });
+
+  test('returns the total number of grains on the board', () => {
+    expect(total().toString()).toEqual('18446744073709551615');
+  });
+});
