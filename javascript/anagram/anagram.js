90c8172cfcc8601540ab71cfcc4b85938e6c7e6a
Shane Laymance <shane.laymance@gmail.com>
2020-10-31 19:56:05 -0700

    Break out sortChars helper function

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -3,15 +3,18 @@
 // convenience to get you started writing code faster.
 //
 
+// Helper function for clarity
+const sortChars = word => [...word].sort().join('');
+
 /**
  * According to Jest, this solution finds all anagrams in the English language in about 150 ms. I'm sure it could be
  * made faster since I'm using .sort() and .join() here.
  */
 export const findAnagrams = (word, anagrams) => {
   const lowerWord = word.toLowerCase();
-  const sortedWord = [...lowerWord].sort().join('');
+  const sortedWord = sortChars(lowerWord);
   return anagrams.filter(anagram => {
     const lowerAnagram = anagram.toLowerCase();
-    return lowerAnagram !== lowerWord && [...lowerAnagram].sort().join('') === sortedWord;
+    return lowerAnagram !== lowerWord && sortChars(lowerAnagram) === sortedWord;
   });
 };
