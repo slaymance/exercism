0a04694798fc355a0680c46e02cfb7c4203c83a3
Shane Laymance <shane.laymance@gmail.com>
2020-11-01 12:08:06 -0800

    Update function comment

--- javascript/acronym/acronym.js
+++ javascript/acronym/acronym.js
@@ -4,8 +4,8 @@
 //
 
 /**
- * This solution splits the phrase on space and - delimiters then creates an acronym based on the first occurrence of a
- * alphabetical character in each string.
+ * This solution splits the phrase on space and - delimiters then creates an acronym based on the first occurrence of
+ * an alphabetical character in each string.
  */
 export const parse = phrase => phrase
   .split(/[\s-]+/)
