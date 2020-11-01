44c75f9ec53636527d62343a5d31090903fa7d8f
Shane Laymance <shane.laymance@gmail.com>
2020-11-01 12:07:11 -0800

    Complete javascript/acronym

---
+++ javascript/acronym/acronym.js
@@ -0,0 +1,12 @@
+//
+// This is only a SKELETON file for the 'Acronym' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+/**
+ * This solution splits the phrase on space and - delimiters then creates an acronym based on the first occurrence of a
+ * alphabetical character in each string.
+ */
+export const parse = phrase => phrase
+  .split(/[\s-]+/)
+  .reduce((acronym, word) => acronym + word.match(/[A-Za-z]/)[0].toUpperCase(), '');
