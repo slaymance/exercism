d8c4006630acbeb893a6dda50bf003445f826679
Shane Laymance <shane.laymance@gmail.com>
2020-11-04 15:21:48 -0800

    Some cleanup

--- javascript/phone-number/phone-number.js
+++ javascript/phone-number/phone-number.js
@@ -4,24 +4,25 @@
 //
 
 const VALID_NUMBER_LENGTH = 10;
+const MAX_NUMBER_LENGTH = 11;
 const COUNTRY_CODE_INDEX = 0;
 const AREA_CODE_INDEX = 0;
 const EXCHANGE_CODE_INDEX = 3;
 
 export const clean = phoneNumber => {
-  if (/[A-Za-z]/.test(phoneNumber)) throw new Error('Letters not permitted');
-  if (/[^\(\)\-\.\+\d\s]/.test(phoneNumber)) throw new Error('Punctuations not permitted');
+  const cleanedNumber = phoneNumber.replace(/[\s\.\-\(\)\+]/g, '');
+  if (/[A-Za-z]/.test(cleanedNumber)) throw new Error('Letters not permitted');
+  if (/\D/.test(cleanedNumber)) throw new Error('Punctuations not permitted');
+  if (cleanedNumber.length < VALID_NUMBER_LENGTH) throw new Error('Incorrect number of digits');
+  if (cleanedNumber.length > MAX_NUMBER_LENGTH) throw new Error(`More than ${MAX_NUMBER_LENGTH} digits`);
+  if (cleanedNumber.length === MAX_NUMBER_LENGTH && cleanedNumber[COUNTRY_CODE_INDEX] !== '1')
+    throw new Error(`${MAX_NUMBER_LENGTH} digits must start with 1`);
 
-  const numbers = phoneNumber.match(/\d/g);
-  if (numbers.length < VALID_NUMBER_LENGTH) throw new Error('Incorrect number of digits');
-  if (numbers.length > 11) throw new Error('More than 11 digits');
-  if (numbers.length === 11 && numbers[COUNTRY_CODE_INDEX] !== '1') throw new Error('11 digits must start with 1');
+  const cleanedNumberLessCountry = cleanedNumber.slice(-VALID_NUMBER_LENGTH);
+  if (cleanedNumberLessCountry[AREA_CODE_INDEX] === '0') throw new Error('Area code cannot start with zero');
+  if (cleanedNumberLessCountry[AREA_CODE_INDEX] === '1') throw new Error('Area code cannot start with one');
+  if (cleanedNumberLessCountry[EXCHANGE_CODE_INDEX] === '0') throw new Error('Exchange code cannot start with zero');
+  if (cleanedNumberLessCountry[EXCHANGE_CODE_INDEX] === '1') throw new Error('Exchange code cannot start with one');
 
-  const numbersLessCountry = numbers.slice(-VALID_NUMBER_LENGTH);
-  if (numbersLessCountry[AREA_CODE_INDEX] === '0') throw new Error('Area code cannot start with zero');
-  if (numbersLessCountry[AREA_CODE_INDEX] === '1') throw new Error('Area code cannot start with one');
-  if (numbersLessCountry[EXCHANGE_CODE_INDEX] === '0') throw new Error('Exchange code cannot start with zero');
-  if (numbersLessCountry[EXCHANGE_CODE_INDEX] === '1') throw new Error('Exchange code cannot start with one');
-
-  return numbersLessCountry.join('')
-};
+  return cleanedNumberLessCountry;
+}
