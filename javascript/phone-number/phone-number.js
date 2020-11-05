95ec0bf6af3019633b0ccfdce2b4027c5e17cfcc
Shane Laymance <shane.laymance@gmail.com>
2020-11-05 08:07:41 -0800

    Make country code a constant

--- javascript/phone-number/phone-number.js
+++ javascript/phone-number/phone-number.js
@@ -5,6 +5,7 @@
 
 const VALID_NUMBER_LENGTH = 10;
 const MAX_NUMBER_LENGTH = 11;
+const VALID_COUNTRY_CODE = '1';
 const COUNTRY_CODE_INDEX = 0;
 const AREA_CODE_INDEX = 0;
 const EXCHANGE_CODE_INDEX = 3;
@@ -15,8 +16,8 @@ export const clean = phoneNumber => {
   if (/\D/.test(cleanedNumber)) throw new Error('Punctuations not permitted');
   if (cleanedNumber.length < VALID_NUMBER_LENGTH) throw new Error('Incorrect number of digits');
   if (cleanedNumber.length > MAX_NUMBER_LENGTH) throw new Error(`More than ${MAX_NUMBER_LENGTH} digits`);
-  if (cleanedNumber.length === MAX_NUMBER_LENGTH && cleanedNumber[COUNTRY_CODE_INDEX] !== '1')
-    throw new Error(`${MAX_NUMBER_LENGTH} digits must start with 1`);
+  if (cleanedNumber.length === MAX_NUMBER_LENGTH && cleanedNumber[COUNTRY_CODE_INDEX] !== VALID_COUNTRY_CODE)
+    throw new Error(`${MAX_NUMBER_LENGTH} digits must start with ${VALID_COUNTRY_CODE}`);
 
   const cleanedNumberLessCountry = cleanedNumber.slice(-VALID_NUMBER_LENGTH);
   if (cleanedNumberLessCountry[AREA_CODE_INDEX] === '0') throw new Error('Area code cannot start with zero');
