3e7c73bf3f336560ca2e307cd8cd14548b1261d6
Shane Laymance <shane.laymance@gmail.com>
2020-11-06 14:31:23 -0800

    Refactor to pipeline operator chain

--- javascript/largest-series-product/largest-series-product.js
+++ javascript/largest-series-product/largest-series-product.js
@@ -5,14 +5,33 @@
 
 import { Series } from '../series/series';
 
-// Helper function for clarity
+// Helper functions for clarity
+const containsNonDigits = string => /\D/.test(string);
 const productBy = array => array.reduce((product, num) => product * num, 1);
 
+/**
+ * These two functions are taken almost directly from my solution to Series exercise here on exercism
+ * https://exercism.io/tracks/javascript/exercises/series/solutions/40ab3d8550d345b580e1aa67a1fd08bf
+ */
+const pullDigits = series => [...series].map(Number);
+const digitSpans = (digits, span) => Array.from(Array(digits.length - span + 1), (_, i) => digits.slice(i, i + span));
+
+/**
+ * My solution uses the experimental pipeline operator which is a TC39 Stage 1 proposal to the JavaScript language. You
+ * can read more about it here:
+ * https://github.com/tc39/proposal-pipeline-operator
+ * 
+ * To run this code you'll need to add the "@babel/plugin-proposal-pipeline-operator" to your dev dependencies and
+ * include the plugin in your babel.config file (the bigint plugin is already included in the project):
+ * plugins: ["@babel/plugin-syntax-bigint", ["@babel/plugin-proposal-pipeline-operator", { "proposal": "smart" }]]
+ */
 export const largestProduct = (series = '', span = 0) => {
   if (span > series.length) throw new Error('Span must be smaller than string length');
   if (span < 0) throw new Error('Span must be greater than zero');
+  if (containsNonDigits(series)) throw new Error('Digits input must only contain digits');
 
-  return new Series(series)
-    .slices(span)
-    .reduce((maxProduct, slice) => Math.max(maxProduct, productBy(slice)), 0);
+  return series
+    |> pullDigits
+    |> digitSpans(#, span)
+    |> #.reduce((maxProduct, slice) => Math.max(maxProduct, productBy(slice)), 0);
 };
