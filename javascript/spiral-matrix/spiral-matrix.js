eb90cec775f5d628ab47fb2af5d7a0ed250a14c6
Shane Laymance <shane.laymance@gmail.com>
2020-11-08 15:41:07 -0800

    Complete javascript/spiral-matrix

---
+++ javascript/spiral-matrix/spiral-matrix.js
@@ -0,0 +1,27 @@
+//
+// This is only a SKELETON file for the 'Spiral Matrix' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class SpiralMatrix {
+  static ofSize(size = 0) {
+    const matrix = Array.from(Array(size), () => []);
+    let num = 1;
+    let min = 0;
+    let max = size - 1;
+    let row = 0;
+    let col = 0;
+
+    while (num < size ** 2) {
+      while (col < max) { matrix[row][col++] = num++ }
+      while (row < max) { matrix[row++][col] = num++ }
+      while (col > min) { matrix[row][col--] = num++ }
+      min++;
+      while (row > min) { matrix[row--][col] = num++ }
+      max--;
+    }
+
+    if (size > 0) matrix[row][col] = num;
+    return matrix;
+  }
+}
