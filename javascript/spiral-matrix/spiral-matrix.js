c4c2b417924a8b98db7881c97f4bb65a7aaa7346
Shane Laymance <shane.laymance@gmail.com>
2020-11-08 15:56:38 -0800

    Add semicolons

--- javascript/spiral-matrix/spiral-matrix.js
+++ javascript/spiral-matrix/spiral-matrix.js
@@ -13,11 +13,11 @@ export class SpiralMatrix {
     let col = 0;
 
     while (num <= size ** 2) {
-      while (col < max) { matrix[row][col++] = num++ }
-      while (row < max) { matrix[row++][col] = num++ }
-      while (col > min) { matrix[row][col--] = num++ }
+      while (col < max) { matrix[row][col++] = num++; }
+      while (row < max) { matrix[row++][col] = num++; }
+      while (col > min) { matrix[row][col--] = num++; }
       min++;
-      while (row > min) { matrix[row--][col] = num++ }
+      while (row > min) { matrix[row--][col] = num++; }
       max--;
       if (num === size ** 2) matrix[row][col] = num++;
     }
