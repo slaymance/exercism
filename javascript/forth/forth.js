d9494a2b2dd30f2b4d8602c27fe4d5b1545603b5
Shane Laymance <shane.laymance@gmail.com>
2020-11-12 15:11:12 -0800

    Complete javascript/forth

---
+++ javascript/forth/forth.js
@@ -0,0 +1,68 @@
+//
+// This is only a SKELETON file for the 'Forth' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+// Helper functions for clarity
+const isNumber = n => typeof n == 'number' && !isNaN(n) && isFinite(n);
+const identity = (...args) => args;
+
+/**
+ * Can you imagine getting this question in an interview? My word...
+ * 
+ * Most of the work remaining is with the #defineOperation method and the operations themselves. Right now, a user
+ * can't define an operation which uses more than one arithmetic operation or multiple different stack operations.
+ * Maybe I'll come back to finish this one day.
+ */
+export class Forth {
+  static validate(num) {
+    if (!isNumber(num)) throw new Error('Stack empty');
+    return num;
+  }
+
+  static #standardOperations = {
+    '+': (num1, num2) => Forth.validate(num1) + Forth.validate(num2),
+    '-': (num1, num2) => Forth.validate(num1) - Forth.validate(num2),
+    '*': (num1, num2) => Forth.validate(num1) * Forth.validate(num2),
+    '/': (num1, num2) => {
+      if (num2 === 0) throw new Error('Division by zero');
+      return Forth.validate(num1) / Forth.validate(num2) | 0;
+    },
+    dup: (...nums) => nums.concat(Forth.validate(nums[nums.length - 1])),
+    drop: (...nums) => isNumber(Forth.validate(nums.pop())) && nums,
+    swap: (num1, num2) => [Forth.validate(num2), Forth.validate(num1)],
+    over: (...nums) => {
+      const [num1, num2] = nums.splice(-2, 2);
+      return [...nums, Forth.validate(num1), Forth.validate(num2), num1];
+    },
+  }
+
+  #stack = [];
+  #operations = { ...Forth.#standardOperations };
+
+  #defineOperation([key, ...ops]) {
+    if (isNumber(+key)) throw new Error('Invalid definition');
+
+    this.#operations[key.toLowerCase()] = ops.reduce((acc, op) => {
+      const operation = isNumber(+op) ?
+        this.#stack.push(+op) && identity :
+        this.#operations[op.toLowerCase()];
+      return (...nums) => operation(...acc(...nums));
+    }, identity);
+  }
+
+  evaluate(expression) {
+    expression.split(' ').forEach((value, i, src) => {
+      const operation = value.toLowerCase();
+      if (operation === ':') return this.#defineOperation(src.splice(i, src.indexOf(';') + 1 - i).slice(1, -1));
+      if (isNumber(+operation)) return this.#stack.push(+operation);
+      if (!(operation in this.#operations)) throw new Error('Unknown command');
+
+      this.#stack = this.#stack.concat(this.#operations[operation](...this.#stack.splice(-2, 2)));
+    });
+  }
+
+  get stack() {
+    return [...this.#stack];
+  }
+}
---
+++ javascript/forth/forth.spec.js
@@ -0,0 +1,265 @@
+import { Forth } from './forth';
+
+describe('Forth', () => {
+  let forth;
+
+  beforeEach(() => {
+    forth = new Forth();
+  });
+
+  describe('parsing and numbers', () => {
+    test('just pushes numbers onto the stack', () => {
+      forth.evaluate('1 2 3 4 5');
+      expect(forth.stack).toEqual([1, 2, 3, 4, 5]);
+    });
+    test('pushes negative numbers onto the stack', () => {
+      forth.evaluate('-1 -2 -3 -4 -5');
+      expect(forth.stack).toEqual([-1, -2, -3, -4, -5]);
+    });
+  });
+
+  describe('addition', () => {
+    test('can add two numbers', () => {
+      forth.evaluate('1 2 +');
+      expect(forth.stack).toEqual([3]);
+    });
+    test('errors if there is only one value on the stack', () => {
+      expect(() => {
+        forth.evaluate('1 +');
+      }).toThrow(new Error('Stack empty'));
+    });
+    test('errors if there is nothing on the stack', () => {
+      expect(() => {
+        forth.evaluate('+');
+      }).toThrow(new Error('Stack empty'));
+    });
+  });
+
+  describe('subtraction', () => {
+    test('can subtract two numbers', () => {
+      forth.evaluate('3 4 -');
+      expect(forth.stack).toEqual([-1]);
+    });
+    test('errors if there is only one value on the stack', () => {
+      expect(() => {
+        forth.evaluate('1 -');
+      }).toThrow(new Error('Stack empty'));
+    });
+    test('errors if there is nothing on the stack', () => {
+      expect(() => {
+        forth.evaluate('-');
+      }).toThrow(new Error('Stack empty'));
+    });
+  });
+
+  describe('multiplication', () => {
+    test('can multiply two numbers', () => {
+      forth.evaluate('2 4 *');
+      expect(forth.stack).toEqual([8]);
+    });
+    test('errors if there is only one value on the stack', () => {
+      expect(() => {
+        forth.evaluate('1 *');
+      }).toThrow(new Error('Stack empty'));
+    });
+    test('errors if there is nothing on the stack', () => {
+      expect(() => {
+        forth.evaluate('*');
+      }).toThrow(new Error('Stack empty'));
+    });
+  });
+
+  describe('division', () => {
+    test('can divide two numbers', () => {
+      forth.evaluate('12 3 /');
+      expect(forth.stack).toEqual([4]);
+    });
+    test('performs integer division', () => {
+      forth.evaluate('8 3 /');
+      expect(forth.stack).toEqual([2]);
+    });
+    test('errors if dividing by zero', () => {
+      expect(() => {
+        forth.evaluate('4 0 /');
+      }).toThrow(new Error('Division by zero'));
+    });
+    test('errors if there is only one value on the stack', () => {
+      expect(() => {
+        forth.evaluate('1 /');
+      }).toThrow(new Error('Stack empty'));
+    });
+    test('errors if there is nothing on the stack', () => {
+      expect(() => {
+        forth.evaluate('/');
+      }).toThrow(new Error('Stack empty'));
+    });
+  });
+
+  describe('combined arithmetic', () => {
+    test('performs addition and subtraction', () => {
+      forth.evaluate('1 2 + 4 -');
+      expect(forth.stack).toEqual([-1]);
+    });
+    test('performs multiplication and division', () => {
+      forth.evaluate('2 4 * 3 /');
+      expect(forth.stack).toEqual([2]);
+    });
+  });
+
+  describe('dup', () => {
+    test('copies a value on the stack', () => {
+      forth.evaluate('1 dup');
+      expect(forth.stack).toEqual([1, 1]);
+    });
+    test('copies the top value on the stack', () => {
+      forth.evaluate('1 2 dup');
+      expect(forth.stack).toEqual([1, 2, 2]);
+    });
+    test('errors if there is nothing on the stack', () => {
+      expect(() => {
+        forth.evaluate('dup');
+      }).toThrow(new Error('Stack empty'));
+    });
+  });
+
+  describe('drop', () => {
+    test('removes the top value on the stack if it is the only one', () => {
+      forth.evaluate('1 drop');
+      expect(forth.stack).toEqual([]);
+    });
+    test('removes the top value on the stack if it is not the only one', () => {
+      forth.evaluate('1 2 drop');
+      expect(forth.stack).toEqual([1]);
+    });
+    test('errors if there is nothing on the stack', () => {
+      expect(() => {
+        forth.evaluate('drop');
+      }).toThrow(new Error('Stack empty'));
+    });
+  });
+
+  describe('swap', () => {
+    test('swaps the top two values on the stack if they are the only ones', () => {
+      forth.evaluate('1 2 swap');
+      expect(forth.stack).toEqual([2, 1]);
+    });
+    test('swaps the top two values on the stack if they are not the only ones', () => {
+      forth.evaluate('1 2 3 swap');
+      expect(forth.stack).toEqual([1, 3, 2]);
+    });
+    test('errors if there is only one value on the stack', () => {
+      expect(() => {
+        forth.evaluate('1 swap');
+      }).toThrow(new Error('Stack empty'));
+    });
+    test('errors if there is nothing on the stack', () => {
+      expect(() => {
+        forth.evaluate('swap');
+      }).toThrow(new Error('Stack empty'));
+    });
+  });
+
+  describe('over', () => {
+    test('copies the second element if there are only two', () => {
+      forth.evaluate('1 2 over');
+      expect(forth.stack).toEqual([1, 2, 1]);
+    });
+    test('copies the second element if there are more than two', () => {
+      forth.evaluate('1 2 3 over');
+      expect(forth.stack).toEqual([1, 2, 3, 2]);
+    });
+    test('errors if there is only one value on the stack', () => {
+      expect(() => {
+        forth.evaluate('1 over');
+      }).toThrow(new Error('Stack empty'));
+    });
+    test('errors if there is nothing on the stack', () => {
+      expect(() => {
+        forth.evaluate('over');
+      }).toThrow(new Error('Stack empty'));
+    });
+  });
+
+  describe('user-defined words', () => {
+    test('can consist of built-in words', () => {
+      forth.evaluate(': dup-twice dup dup ;');
+      forth.evaluate('1 dup-twice');
+      expect(forth.stack).toEqual([1, 1, 1]);
+    });
+    test('execute in the right order', () => {
+      forth.evaluate(': countup 1 2 3 ;');
+      forth.evaluate('countup');
+      expect(forth.stack).toEqual([1, 2, 3]);
+    });
+    test('can override other user-defined words', () => {
+      forth.evaluate(': foo dup ;');
+      forth.evaluate(': foo dup dup ;');
+      forth.evaluate('1 foo');
+      expect(forth.stack).toEqual([1, 1, 1]);
+    });
+    test('can override built-in words', () => {
+      forth.evaluate(': swap dup ;');
+      forth.evaluate('1 swap');
+      expect(forth.stack).toEqual([1, 1]);
+    });
+    test('can override built-in operators', () => {
+      forth.evaluate(': + * ;');
+      forth.evaluate('3 4 +');
+      expect(forth.stack).toEqual([12]);
+    });
+    test('cannot redefine numbers', () => {
+      expect(() => {
+        forth.evaluate(': 1 2 ;');
+      }).toThrow(new Error('Invalid definition'));
+    });
+    test('errors if executing a non-existent word', () => {
+      expect(() => {
+        forth.evaluate('foo');
+      }).toThrow(new Error('Unknown command'));
+    });
+    test('only defines words for current instance', () => {
+      const first = new Forth();
+      const second = new Forth();
+      first.evaluate(': + - ;');
+      first.evaluate('1 1 +');
+      second.evaluate('1 1 +');
+      expect(first.stack).toEqual([0]);
+      expect(second.stack).toEqual([2]);
+    });
+
+    test('double over', () => {
+      forth.evaluate(': double-over over over ;');
+      forth.evaluate('3 4 5 6 double-over');
+      expect(forth.stack).toEqual([3, 4, 5, 6, 5, 6])
+    })
+  });
+
+  describe('case-insensitivity', () => {
+    test('DUP is case-insensitive', () => {
+      forth.evaluate('1 DUP Dup dup');
+      expect(forth.stack).toEqual([1, 1, 1, 1]);
+    });
+    test('DROP is case-insensitive', () => {
+      forth.evaluate('1 2 3 4 DROP Drop drop');
+      expect(forth.stack).toEqual([1]);
+    });
+    test('SWAP is case-insensitive', () => {
+      forth.evaluate('1 2 SWAP 3 Swap 4 swap');
+      expect(forth.stack).toEqual([2, 3, 4, 1]);
+    });
+    test('OVER is case-insensitive', () => {
+      forth.evaluate('1 2 OVER Over over');
+      expect(forth.stack).toEqual([1, 2, 1, 2, 1]);
+    });
+    test('user-defined words are case-insensitive', () => {
+      forth.evaluate(': foo dup ;');
+      forth.evaluate('1 FOO Foo foo');
+      expect(forth.stack).toEqual([1, 1, 1, 1]);
+    });
+    test('definitions are case-insensitive', () => {
+      forth.evaluate(': SWAP DUP Dup dup ;');
+      forth.evaluate('1 swap');
+      expect(forth.stack).toEqual([1, 1, 1, 1]);
+    });
+  });
+});
