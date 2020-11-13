80a8a7d379fe391b9d40d57dd22072be40237a4d
Shane Laymance <shane.laymance@gmail.com>
2020-11-13 10:57:57 -0800

    Factor out WRIGGLED

--- javascript/food-chain/food-chain.js
+++ javascript/food-chain/food-chain.js
@@ -6,9 +6,10 @@
 // Helper functions for clarity
 const range = (min, max) => [...Array(max + 1).keys()].slice(min, max + 1);
 
+const WRIGGLED = 'wriggled and jiggled and tickled inside her';
 const ANIMALS = [
   ['fly', 'I don\'t know why she swallowed the fly. Perhaps she\'ll die.\n'],
-  ['spider', 'It wriggled and jiggled and tickled inside her.\n'],
+  ['spider', `It ${WRIGGLED}.\n`],
   ['bird', 'How absurd to swallow a bird!\n'],
   ['cat', 'Imagine that, to swallow a cat!\n'],
   ['dog', 'What a hog, to swallow a dog!\n'],
@@ -19,20 +20,20 @@ const ANIMALS = [
 
 const getAnimal = num => ANIMALS[num][0];
 const getAnimalLine = num => ANIMALS[num][1];
-const know = animal => `I know an old lady who swallowed a ${animal}.\n`;
-const swallow = (swallowed, caught) => `She swallowed the ${swallowed} to catch the ${caught}${caught === 'spider' ?
-  ' that wriggled and jiggled and tickled inside her' : ''}.\n`;
+const know = num => `I know an old lady who swallowed a ${getAnimal(num)}.\n`;
+const swallow = num =>
+  `She swallowed the ${getAnimal(num + 1)} to catch the ${getAnimal(num)}${getAnimal(num) === 'spider' ?
+    ` that ${WRIGGLED}` : ''}.\n`;
 
 /**
  * I refactored the code and the tests to use functions instead of a class. Since we're not managing state within the
  * class, functions are sufficient.
  */
 export const verse = num => {
-  const firstLines = know(getAnimal(num - 1)) + getAnimalLine(num - 1);
+  const firstLines = know(num - 1) + getAnimalLine(num - 1);
   if (num === 1 || num === 8) return firstLines;
 
-  return range(0, num - 2).reduceRight((verse, line) =>
-    verse + swallow(getAnimal(line + 1), getAnimal(line)), firstLines) + getAnimalLine(0);
+  return range(0, num - 2).reduceRight((verse, line) => verse + swallow(line), firstLines) + getAnimalLine(0);
 };
 
 export const verses = (first, last) => range(first, last).reduce((song, verseNum) => song + verse(verseNum) + '\n', '');
