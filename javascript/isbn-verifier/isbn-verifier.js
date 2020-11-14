a014db770054cff93042b5e91f024f6d0de8bdad
Shane Laymance <shane.laymance@gmail.com>
2020-11-14 13:48:38 -0800

    Just make it one function

--- javascript/isbn-verifier/isbn-verifier.js
+++ javascript/isbn-verifier/isbn-verifier.js
@@ -3,9 +3,10 @@
 // convenience to get you started writing code faster.
 //
 
-const verify = isbn => [
-  digits => digits.length === 10,
-  digits => digits.reverse().reduce((sum, digit, i) => sum + (digit === 'X' ? 10 : digit) * (i + 1), 0) % 11 === 0
-].every(check => check(isbn));
-
-export const isValid = isbn => verify(isbn.match(/\d|X$/g) || []);
+export const isValid = isbn => {
+  const parsedISBN = isbn.match(/\d|X$/g) || [];
+  return [
+    digits => digits.length === 10,
+    digits => digits.reduce((sum, digit, i) => sum + (digit === 'X' ? 10 : digit) * (10 - i), 0) % 11 === 0
+  ].every(check => check(parsedISBN));
+};
