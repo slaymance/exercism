d5a2a0b58e0aa135b88f085039459cfde73aecde
Shane Laymance <shane.laymance@gmail.com>
2020-11-15 10:32:35 -0800

    Change [a-z] to \w and update comment

--- javascript/pig-latin/pig-latin.js
+++ javascript/pig-latin/pig-latin.js
@@ -6,8 +6,11 @@
  * This solution would not work for any words that have an 'xr' occurring after the start of the word but before any
  * other vowels occur. Luckily, I don't think any such words exist. I also don't deal with capitalization and
  * punctuation.
+ * 
+ * Also, it's a bit unclear if a word like 'query' would follow rule 3. If that's the case and it should be translated
+ * to 'ueryqay', then the u regex would be changed to (?<!\wq)u to make sure a consonant sound precedes 'qu'.
  */
-const firstVowelIndex = string => string.search(/[aeio]|(?<!q)u|(?<=[a-z])y|xr|yt/i);
+const firstVowelIndex = string => string.search(/[aeio]|(?<!q)u|(?<=\w)y|xr|yt/i);
 const splitAtIndex = (string, index) => string.slice(index) + string.slice(0, index);
 
 export const translate = string => string
