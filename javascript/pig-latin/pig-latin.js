12c5f69a14244449687f87473d06175c6837d26a
Shane Laymance <shane.laymance@gmail.com>
2020-11-15 10:02:33 -0800

    Get rid of startsWithVowel function

--- javascript/pig-latin/pig-latin.js
+++ javascript/pig-latin/pig-latin.js
@@ -1,23 +1,18 @@
-//
-// This is only a SKELETON file for the 'Pig Latin' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 /**
  * As with all language parsing problems, it comes down to arcane regular expressions.
  * ?<! is a negative lookbehind, so (?<!q)u will only match a 'u' that is not preceded by a 'q'.
  * ?<= is a positive lookbehind, so (?<=[a-z])y will only match a 'y' that is preceded by another letter.
- * For startsWithVowel, I start each vowel in the regular expression with ^ which tests if the vowel occurs at the
- * beginning of the string.
+ * 
+ * This solution would not work for any words that have an 'xr' occurring after the start of the word but before any
+ * other vowels occur. Luckily, I don't think any such words exist. I also don't deal with capitalization and
+ * punctuation.
  */
 const VOWELS = ['[aeio]', '(?<!q)u', '(?<=[a-z])y', 'xr', 'yt'];
 
-const startsWithVowel = string => new RegExp(`^${VOWELS.join('|^')}`, 'i').test(string);
 const firstVowelIndex = string => string.search(new RegExp(VOWELS.join('|'), 'i'));
 const splitAtIndex = (string, index) => string.slice(index) + string.slice(0, index);
 
 export const translate = string => string
   .split(' ')
-  .map(word => (startsWithVowel(word) ? word : splitAtIndex(word, firstVowelIndex(word))) + 'ay')
+  .map(word => splitAtIndex(word, firstVowelIndex(word)) + 'ay')
   .join(' ');
-
