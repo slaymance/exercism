a32a1290e5510f15c3269e3b9f642fe1524e76b8
Shane Laymance <shane.laymance@gmail.com>
2020-11-16 05:00:34 -0800

    Refactor to get rid of isString helper

--- javascript/proverb/proverb.js
+++ javascript/proverb/proverb.js
@@ -3,12 +3,9 @@
 // convenience to get you started writing code faster.
 //
 
-// Helper function for clarity
-const isString = value => typeof value === 'string';
-
 const line = (object, subject) => `For want of a ${object} the ${subject} was lost.\n`;
 const finalLine = (object, qualifier) => `And all for the want of a ${(qualifier ? qualifier + ' ' : '') + object}.`
 
-export const proverb = (...inputs) => inputs.slice(0, -1).reduce(
-  (result, object, i) => result + (isString(inputs[i + 1]) ? line(object, inputs[i + 1]) : ''), ''
+export const proverb = (...inputs) => inputs.slice(0, inputs[inputs.length - 1]?.qualifier ? -2 : -1).reduce(
+  (result, object, i) => result + line(object, inputs[i + 1]), ''
 ) + finalLine(inputs[0], inputs[inputs.length - 1]?.qualifier);
