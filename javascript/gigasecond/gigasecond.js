5bd3eb319792aa94061ded248b6f4cd03108b50c
Shane Laymance <shane.laymance@gmail.com>
2020-11-16 05:21:00 -0800

    Complete javascript/gigasecond

---
+++ javascript/gigasecond/gigasecond.js
@@ -0,0 +1,8 @@
+//
+// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+const GIGASECOND = 1000000000000;
+
+export const gigasecond = date => new Date(date.getTime() + GIGASECOND);
