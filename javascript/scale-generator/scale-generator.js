2752d9e73687219247f89513e2e8bb1884c19677
Shane Laymance <shane.laymance@gmail.com>
2020-11-26 11:50:40 -0800

    Complete javascript/scale-generator

---
+++ javascript/scale-generator/scale-generator.js
@@ -0,0 +1,39 @@
+//
+// This is only a SKELETON file for the 'Scale Generator' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+/**
+ * I evaluate the chromatic scale in the constructor. Whenever asked for an interval, I calculate the chromatic indeces
+ * of the notes present in the scale and pull those elements.
+ */
+export class Scale {
+  static FLAT = 'b';
+  static SHARP = '#';
+  static STEPS = {
+    m: 1,
+    M: 2,
+    A: 3
+  };
+
+  #chromatic;
+
+  constructor(tonic) {
+    const { FLAT, SHARP } = Scale;
+    const usesFlats = tonic[1] === FLAT || ['F', 'd', 'g', 'c', 'f'].includes(tonic);
+    const chromatic = ['A', '', 'B', 'C', '', 'D', '', 'E', 'F', '', 'G', ''].map((note, i, src) =>
+      note || (usesFlats ? src[(i + 1) % src.length] + FLAT : src[i - 1] + SHARP));
+    this.#chromatic = chromatic.splice(chromatic.indexOf(tonic[0].toUpperCase() + (tonic[1] || ''))).concat(chromatic);
+  }
+
+  chromatic() {
+    return [...this.#chromatic];
+  }
+
+  interval([...intervals]) {
+    return intervals
+      .slice(0, -1) // We don't need the last interval since we always start with the 0 index of chromatic scale.
+      .reduce((acc, step) => acc.concat(acc[acc.length - 1] + Scale.STEPS[step]), [0])
+      .map(i => this.#chromatic[i]);
+  }
+}
---
+++ javascript/scale-generator/scale-generator.spec.js
@@ -0,0 +1,92 @@
+import { Scale } from './scale-generator'
+
+describe('ScaleGenerator', () => {
+  describe('Chromatic scales', () => {
+    test('Chromatic scale with sharps', () => {
+      const expected = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+      expect(new Scale('C').chromatic()).toEqual(expected)
+    })
+
+    test('Chromatic scale with flats', () => {
+      const expected = ['F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E']
+      expect(new Scale('F').chromatic()).toEqual(expected)
+    })
+  })
+
+  describe('Scales with specified intervals', () => {
+    test('Simple major scale', () => {
+      const expected = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
+      expect(new Scale('C').interval('MMmMMMm')).toEqual(expected)
+    })
+
+    test('Major scale with sharps', () => {
+      const expected = ['G', 'A', 'B', 'C', 'D', 'E', 'F#']
+      expect(new Scale('G').interval('MMmMMMm')).toEqual(expected)
+    })
+
+    test('Major scale with flats', () => {
+      const expected = ['F', 'G', 'A', 'Bb', 'C', 'D', 'E']
+      expect(new Scale('F').interval('MMmMMMm')).toEqual(expected)
+    })
+
+    test('Minor scale with sharps', () => {
+      const expected = ['F#', 'G#', 'A', 'B', 'C#', 'D', 'E']
+      expect(new Scale('f#').interval('MmMMmMM')).toEqual(expected)
+    })
+
+    test('Minor scale with flats', () => {
+      const expected = ['Bb', 'C', 'Db', 'Eb', 'F', 'Gb', 'Ab']
+      expect(new Scale('bb').interval('MmMMmMM')).toEqual(expected)
+    })
+
+    test('Dorian mode', () => {
+      const expected = ['D', 'E', 'F', 'G', 'A', 'B', 'C']
+      expect(new Scale('d').interval('MmMMMmM')).toEqual(expected)
+    })
+
+    test('Mixolydian mode', () => {
+      const expected = ['Eb', 'F', 'G', 'Ab', 'Bb', 'C', 'Db']
+      expect(new Scale('Eb').interval('MMmMMmM')).toEqual(expected)
+    })
+
+    test('Lydian mode', () => {
+      const expected = ['A', 'B', 'C#', 'D#', 'E', 'F#', 'G#']
+      expect(new Scale('a').interval('MMMmMMm')).toEqual(expected)
+    })
+
+    test('Phrygian mode', () => {
+      const expected = ['E', 'F', 'G', 'A', 'B', 'C', 'D']
+      expect(new Scale('e').interval('mMMMmMM')).toEqual(expected)
+    })
+
+    test('Locrian mode', () => {
+      const expected = ['G', 'Ab', 'Bb', 'C', 'Db', 'Eb', 'F']
+      expect(new Scale('g').interval('mMMmMMM')).toEqual(expected)
+    })
+
+    test('Harmonic minor', () => {
+      const expected = ['D', 'E', 'F', 'G', 'A', 'Bb', 'Db']
+      expect(new Scale('d').interval('MmMMmAm')).toEqual(expected)
+    })
+
+    test('Octatonic', () => {
+      const expected = ['C', 'D', 'D#', 'F', 'F#', 'G#', 'A', 'B']
+      expect(new Scale('C').interval('MmMmMmMm')).toEqual(expected)
+    })
+
+    test('Hexatonic', () => {
+      const expected = ['Db', 'Eb', 'F', 'G', 'A', 'B']
+      expect(new Scale('Db').interval('MMMMMM')).toEqual(expected)
+    })
+
+    test('Pentatonic', () => {
+      const expected = ['A', 'B', 'C#', 'E', 'F#']
+      expect(new Scale('A').interval('MMAMA')).toEqual(expected)
+    })
+
+    test('Enigmatic', () => {
+      const expected = ['G', 'G#', 'B', 'C#', 'D#', 'F', 'F#']
+      expect(new Scale('G').interval('mAMMMmm')).toEqual(expected)
+    })
+  })
+})
