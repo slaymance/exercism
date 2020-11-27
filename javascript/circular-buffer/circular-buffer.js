71b4583cca734a3591683d845d16a05f4866cbdc
Shane Laymance <shane.laymance@gmail.com>
2020-11-27 12:21:58 -0800

    Refactor to circular buffer w/ read/write indeces

--- javascript/circular-buffer/circular-buffer.js
+++ javascript/circular-buffer/circular-buffer.js
@@ -3,37 +3,70 @@
 // convenience to get you started writing code faster.
 //
 
-// Helper function
+// Helpers
 const isNil = value => value == null;
+class Null { } // This is for resetting a value after reading since null values can't be written
 
 class CircularBuffer {
-  #size;
-  #values = [];
+  #values;
+  #readIndex = 0;
+  #writeIndex = 0;
 
   constructor(size) {
-    this.#size = size;
+    this.#values = Array(size);
+  }
+
+  #increment(index) {
+    return (index + 1) % this.#values.length;
+  }
+
+  #incrementRead() {
+    this.#readIndex = this.#increment(this.#readIndex);
+  }
+
+  #incrementWrite() {
+    this.#writeIndex = this.#increment(this.#writeIndex);
+  }
+
+  get #isFull() {
+    return !isNil(this.#values[this.#writeIndex]);
+  }
+
+  get #isEmpty() {
+    return isNil(this.#values[this.#readIndex]);
+  }
+
+  #setValue(value, useWriteIndex) {
+    if (isNil(value)) return;
+    this.#values[useWriteIndex ? this.#writeIndex : this.#readIndex] = value instanceof Null ? null : value;
+    useWriteIndex ? this.#incrementWrite() : this.#incrementRead();
   }
 
   write(value) {
-    if (this.#values.length === this.#size) throw new BufferFullError();
-    !isNil(value) && this.#values.push(value);
+    if (this.#isFull) throw new BufferFullError();
+    this.#setValue(value, true);
   }
 
   read() {
-    if (!this.#values.length) throw new BufferEmptyError();
-    return this.#values.shift();
+    if (this.#isEmpty) throw new BufferEmptyError();
+
+    const value = this.#values[this.#readIndex];
+    if (this.#isFull) this.#writeIndex = this.#readIndex;
+    this.#setValue(new Null(), false);
+    return value;
   }
 
   forceWrite(value) {
     try {
       this.write(value);
     } catch {
-      if (!isNil(value)) this.#values = this.#values.slice(1).concat(value);
+      this.#setValue(value, false);
     }
   }
 
   clear() {
-    this.#values = [];
+    this.#values.fill(null);
+    this.#readIndex = this.#writeIndex = 0;
   }
 }
 
