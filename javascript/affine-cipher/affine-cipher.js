08d5b75deb04a767c4e267d6f426e0bee75eca80
Shane Laymance <shane.laymance@gmail.com>
2020-12-08 12:52:05 -0800

    Refactor chunkString to regex match

--- javascript/affine-cipher/affine-cipher.js
+++ javascript/affine-cipher/affine-cipher.js
@@ -5,10 +5,6 @@ const CHUNK_SIZE = 5;
 const areCoprime = (a, b) => a > 2 && b > 2 &&
   [...Array(Math.min(a, b) + 1).keys()].slice(2).every(factor => a % factor || b % factor);
 
-const chunkString = (size => string => string.length ?
-  [string.slice(0, size), ...chunkString(string.slice(size), size)] :
-  [])(CHUNK_SIZE);
-
 const findMMI = (a, m) => {
   if (!areCoprime(a, m)) throw new Error('a and m must be coprime.');
 
@@ -22,18 +18,23 @@ const transpose = encryptFn => char => String.fromCharCode(encryptFn(char) % NUM
 export const encode = (phrase, { a, b }) => {
   if (!areCoprime(a, NUMBER_OF_LETTERS)) throw new Error('a and m must be coprime.');
 
-  return chunkString(phrase.toLowerCase().replace(/\W/g, ''))
-    .map(chunk => chunk.replace(
+  return phrase
+    .toLowerCase()
+    .replace(/\W/g, '')
+    .replace(
       /[a-z]/g,
-      transpose(char => a * (char.charCodeAt() - ASCII_START) + b)))
+      transpose(char => a * (char.charCodeAt() - ASCII_START) + b))
+    .match(new RegExp(`.{1,${CHUNK_SIZE}}`, 'g'))
     .join(' ');
 };
 
 export const decode = (phrase, { a, b }) => {
   const mmi = findMMI(a, NUMBER_OF_LETTERS);
 
-  return phrase.replace(/\W/g, '').replace(
-    /[a-z]/g,
-    transpose(char => mmi * (char.charCodeAt() - ASCII_START - b) % NUMBER_OF_LETTERS + NUMBER_OF_LETTERS)
-  );
+  return phrase
+    .replace(/\W/g, '')
+    .replace(
+      /[a-z]/g,
+      transpose(char => mmi * (char.charCodeAt() - ASCII_START - b) % NUMBER_OF_LETTERS + NUMBER_OF_LETTERS)
+    );
 };
