34b48104ca54d6255b170ea54415f819e15fcc08
Shane Laymance <shane.laymance@gmail.com>
2020-12-09 10:54:41 -0800

    Convert to pipeline chain

--- javascript/atbash-cipher/atbash-cipher.js
+++ javascript/atbash-cipher/atbash-cipher.js
@@ -7,15 +7,21 @@ const ASCII_START = 'a'.charCodeAt();
 const ASCII_END = 'z'.charCodeAt();
 const CHUNK_SIZE = 5;
 
-const transpose = char => String.fromCharCode(ASCII_START + ASCII_END - char.charCodeAt());
-
+/**
+ * My solution uses the experimental pipeline operator which is a TC39 Stage 1 proposal to the JavaScript language. You
+ * can read more about it here:
+ * https://github.com/tc39/proposal-pipeline-operator
+ * 
+ * To run this code you'll need to add the "@babel/plugin-proposal-pipeline-operator" to your dev dependencies and
+ * include the plugin in your babel.config file (the bigint plugin is already included in the project):
+ * plugins: ["@babel/plugin-syntax-bigint", ["@babel/plugin-proposal-pipeline-operator", { "proposal": "smart" }]]
+ */
 export const encode = string => string
-  .toLowerCase()
-  .replace(/\W/g, '')
-  .replace(/[a-z]/g, transpose)
-  .match(new RegExp(`.{1,${CHUNK_SIZE}}`, 'g'))
-  .join(' ');
+  |> #.toLowerCase()
+  |> decode(#)
+  |> #.match(new RegExp(`.{1,${CHUNK_SIZE}}`, 'g'))
+  |> #.join(' ');
 
 export const decode = string => string
   .replace(/\W/g, '')
-  .replace(/[a-z]/g, transpose);
+  .replace(/[a-z]/g, char => String.fromCharCode(ASCII_START + ASCII_END - char.charCodeAt()));
