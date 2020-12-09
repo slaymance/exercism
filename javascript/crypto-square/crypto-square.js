71641a240c22e9e048ba9564e7a68479dc9d8927
Shane Laymance <shane.laymance@gmail.com>
2020-12-09 11:39:03 -0800

    Complete javascript/crypto-square

---
+++ javascript/crypto-square/crypto-square.js
@@ -0,0 +1,36 @@
+//
+// This is only a SKELETON file for the 'Crypto Square' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+/**
+ * I feel like the tests don't really match what the instructions talked about. Like, the instructions
+ * say that size is r x c, but in the implementation size is just c. It also talks about padding rows with
+ * trailing spaces which the tests don't ask for. I don't know, I didn't put much thought into this one.
+ */
+export class Crypto {
+  #normalizedText;
+
+  constructor(text) {
+    this.#normalizedText = text.toLowerCase().replace(/\W/g, '');
+  }
+
+  normalizePlaintext() {
+    return this.#normalizedText;
+  }
+
+  size() {
+    return Math.ceil(Math.sqrt(this.normalizePlaintext().length));
+  }
+
+  plaintextSegments() {
+    return this.normalizePlaintext().match(new RegExp(`.{1,${this.size()}}`, 'g'));
+  }
+
+  ciphertext() {
+    const segments = this.plaintextSegments();
+    return [...Array(this.size()).keys()]
+      .flatMap(col => segments.map((_, row, src) => src[row][col]))
+      .join('')
+  }
+}
