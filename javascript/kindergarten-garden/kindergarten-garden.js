c543e0aeccd1c480b8074b9e7c08e505dfae2ac6
Shane Laymance <shane.laymance@gmail.com>
2020-12-10 01:47:26 -0800

    Add slice to maintain immutability

--- javascript/kindergarten-garden/kindergarten-garden.js
+++ javascript/kindergarten-garden/kindergarten-garden.js
@@ -30,7 +30,7 @@ const PLANTS = {
  */
 export const studentPlants = (cups, students) => {
   const rows = cups.split('\n');
-  const sortedStudents = students?.sort() ?? STUDENTS;
+  const sortedStudents = students?.slice().sort() ?? STUDENTS;
   return student => {
     const cupNum = sortedStudents.indexOf(student) * 2;
     return rows.flatMap(row => [...row.slice(cupNum, cupNum + 2)]).map(cup => PLANTS[cup]);
