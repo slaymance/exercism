3402988e6e9f907fc215dab1fa54333123bbdf78
Shane Laymance <shane.laymance@gmail.com>
2020-12-10 02:08:07 -0800

    Complete javascript/sum-of-multiples

---
+++ javascript/sum-of-multiples/sum-of-multiples.js
@@ -0,0 +1,8 @@
+//
+// This is only a SKELETON file for the 'Sum Of Multiples' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const sum = (factors, max) => [...Array(max).keys()]
+  .slice(Math.min(...factors))
+  .reduce((multipleSum, multiple) => multipleSum + (factors.some(factor => multiple % factor === 0) && multiple), 0);
