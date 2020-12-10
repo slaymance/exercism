7084fcbba26da40830647256937eabf508249b43
Shane Laymance <shane.laymance@gmail.com>
2020-12-10 00:46:52 -0800

    Complete javascript/robot-simulator

---
+++ javascript/robot-simulator/robot-simulator.js
@@ -0,0 +1,73 @@
+//
+// This is only a SKELETON file for the 'Robot Simulator' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class InvalidInputError extends Error { }
+
+export class Robot {
+  static NORTH = 'north';
+  static EAST = 'east';
+  static SOUTH = 'south';
+  static WEST = 'west';
+  static BEARINGS = [Robot.NORTH, Robot.EAST, Robot.SOUTH, Robot.WEST];
+  static INSTRUCTIONS = {
+    A: 'advance',
+    L: 'turnLeft',
+    R: 'turnRight'
+  };
+
+  static instructions(chain) {
+    return [...chain].map(instruction => Robot.INSTRUCTIONS[instruction]);
+  }
+
+  #bearing;
+  #coordinates;
+
+  get bearing() {
+    return this.#bearing;
+  }
+
+  get coordinates() {
+    return [...this.#coordinates];
+  }
+
+  orient(bearing) {
+    if (!Robot.BEARINGS.includes(bearing)) throw new InvalidInputError();
+    this.#bearing = bearing;
+  }
+
+  at(...coordinates) {
+    this.#coordinates = coordinates;
+  }
+
+  turnRight() {
+    this.orient(Robot.BEARINGS[(Robot.BEARINGS.indexOf(this.#bearing) + 1) % Robot.BEARINGS.length]);
+  }
+
+  turnLeft() {
+    this.orient(Robot.BEARINGS[(Robot.BEARINGS.indexOf(this.#bearing) + 3) % Robot.BEARINGS.length]);
+  }
+
+  advance() {
+    switch (this.bearing) {
+      case Robot.NORTH:
+        return this.#coordinates[1]++;
+      case Robot.EAST:
+        return this.#coordinates[0]++;
+      case Robot.SOUTH:
+        return this.#coordinates[1]--;
+      case Robot.WEST:
+        return this.#coordinates[0]--;
+    }
+  }
+
+  place({ direction, x, y }) {
+    this.orient(direction);
+    this.at(x, y)
+  }
+
+  evaluate(chain) {
+    Robot.instructions(chain).forEach(instruction => this[instruction]());
+  }
+}
---
+++ javascript/robot-simulator/robot-simulator.spec.js
@@ -0,0 +1,150 @@
+import { Robot, InvalidInputError } from './robot-simulator';
+
+describe('Robot', () => {
+  const robot = new Robot();
+
+  test('robot bearing', () => {
+    const directions = ['east', 'west', 'north', 'south'];
+
+    directions.forEach((currentDirection) => {
+      robot.orient(currentDirection);
+      expect(robot.bearing).toEqual(currentDirection);
+    });
+  });
+
+  test('invalid robot bearing', () => {
+    expect(InvalidInputError.prototype).toBeInstanceOf(Error);
+    expect(() => robot.orient('crood')).toThrow(InvalidInputError);
+  });
+
+  test('turn right from north', () => {
+    robot.orient('north');
+    robot.turnRight();
+    expect(robot.bearing).toEqual('east');
+  });
+
+  test('turn right from east', () => {
+    robot.orient('east');
+    robot.turnRight();
+    expect(robot.bearing).toEqual('south');
+  });
+
+  test('turn right from south', () => {
+    robot.orient('south');
+    robot.turnRight();
+    expect(robot.bearing).toEqual('west');
+  });
+
+  test('turn right from west', () => {
+    robot.orient('west');
+    robot.turnRight();
+    expect(robot.bearing).toEqual('north');
+  });
+
+  test('turn left from north', () => {
+    robot.orient('north');
+    robot.turnLeft();
+    expect(robot.bearing).toEqual('west');
+  });
+
+  test('turn left from east', () => {
+    robot.orient('east');
+    robot.turnLeft();
+    expect(robot.bearing).toEqual('north');
+  });
+
+  test('turn left from south', () => {
+    robot.orient('south');
+    robot.turnLeft();
+    expect(robot.bearing).toEqual('east');
+  });
+
+  test('turn left from west', () => {
+    robot.orient('west');
+    robot.turnLeft();
+    expect(robot.bearing).toEqual('south');
+  });
+
+  test('robot coordinates', () => {
+    robot.at(3, 0);
+    expect(robot.coordinates).toEqual([3, 0]);
+  });
+
+  test('other robot coordinates', () => {
+    robot.at(-2, 5);
+    expect(robot.coordinates).toEqual([-2, 5]);
+  });
+
+  test('advance when facing north', () => {
+    robot.at(0, 0);
+    robot.orient('north');
+    robot.advance();
+    expect(robot.coordinates).toEqual([0, 1]);
+  });
+
+  test('advance when facing east', () => {
+    robot.at(0, 0);
+    robot.orient('east');
+    robot.advance();
+    expect(robot.coordinates).toEqual([1, 0]);
+  });
+
+  test('advance when facing south', () => {
+    robot.at(0, 0);
+    robot.orient('south');
+    robot.advance();
+    expect(robot.coordinates).toEqual([0, -1]);
+  });
+
+  test('advance when facing west', () => {
+    robot.at(0, 0);
+    robot.orient('west');
+    robot.advance();
+    expect(robot.coordinates).toEqual([-1, 0]);
+  });
+
+  test('instructions for turning left', () => {
+    expect(Robot.instructions('L')).toEqual(['turnLeft']);
+  });
+
+  test('instructions for turning right', () => {
+    expect(Robot.instructions('R')).toEqual(['turnRight']);
+  });
+
+  test('instructions for advancing', () => {
+    expect(Robot.instructions('A')).toEqual(['advance']);
+  });
+
+  test('series of instructions', () => {
+    expect(Robot.instructions('RAAL'))
+      .toEqual(['turnRight', 'advance', 'advance', 'turnLeft']);
+  });
+
+  test('instruct robot', () => {
+    robot.place({ x: -2, y: 1, direction: 'east' });
+    robot.evaluate('RLAALAL');
+    expect(robot.coordinates).toEqual([0, 2]);
+    expect(robot.bearing).toEqual('west');
+  });
+
+  test('instruct many robots', () => {
+    const robot1 = new Robot();
+    const robot2 = new Robot();
+    const robot3 = new Robot();
+    robot1.place({ x: 0, y: 0, direction: 'north' });
+    robot2.place({ x: 2, y: -7, direction: 'east' });
+    robot3.place({ x: 8, y: 4, direction: 'south' });
+    robot1.evaluate('LAAARALA');
+    robot2.evaluate('RRAAAAALA');
+    robot3.evaluate('LAAARRRALLLL');
+
+    expect(robot1.coordinates).toEqual([-4, 1]);
+    expect(robot1.bearing).toEqual('west');
+
+    expect(robot2.coordinates).toEqual([-3, -8]);
+    expect(robot2.bearing).toEqual('south');
+
+    expect(robot3.coordinates).toEqual([11, 5]);
+    expect(robot3.bearing).toEqual('north');
+  });
+});
