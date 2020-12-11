3e2b1d4da05558fdfe4e0890fe5ea3fa79bdd9dc
Shane Laymance <shane.laymance@gmail.com>
2020-12-11 11:34:22 -0800

    Complete javascript/darts

---
+++ javascript/darts/darts.js
@@ -0,0 +1,22 @@
+//
+// This is only a SKELETON file for the 'Darts' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+/**
+ * I chose a simple array to represent each circle: the 0 index of each element
+ * represents the radius and the 1 index represents the points. The radii must
+ * be in increasing order for my .find solution to work.
+ * 
+ * The solution is simple: find the distance from the origin to the provided
+ * dart's position and return the score of the circle with radius that is closest
+ * to, but not less than, that distance.
+ */
+const POINTS = [
+  [1, 10],
+  [5, 5],
+  [10, 1],
+  [Infinity, 0]
+];
+
+export const score = (x, y) => POINTS.find(circle => Math.sqrt(x ** 2 + y ** 2) <= circle[0])[1];
