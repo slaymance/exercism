5ce3a28a54f63df0525c3bfc27fd6ae3c565be4b
Shane Laymance <shane.laymance@gmail.com>
2020-12-12 12:50:46 -0800

    Refactor to recursive base conversion function

--- javascript/all-your-base/all-your-base.js
+++ javascript/all-your-base/all-your-base.js
@@ -3,7 +3,10 @@
 // convenience to get you started writing code faster.
 //
 
-const verifyInputs = (digits, a = 0, b = 0) => {
+const convertToBase = (num, base, numLength = 1 + ~~(Math.log(num) / Math.log(base))) =>
+  numLength ? convertToBase(num / base | 0, base, numLength - 1).concat(num % base) : [];
+
+export const convert = (digits, a = 0, b = 0) => {
   if (a <= 1 || a % 1) throw new Error('Wrong input base');
   if (b <= 1 || b % 1) throw new Error('Wrong output base');
   if ([
@@ -11,19 +14,7 @@ const verifyInputs = (digits, a = 0, b = 0) => {
     digits.length > 1 && digits[0] === 0,
     digits.some(digit => digit < 0 || digit >= a),
   ].some(assertion => assertion)) throw new Error('Input has wrong format');
-};
 
-export const convert = (digits, a, b) => {
-  verifyInputs(digits, a, b);
-
-  let remainder = digits.reduce((num, digit, i, { length }) => num + digit * a ** (length - i - 1), 0);
-  const result = Array(1 + ~~(Math.log(remainder) / Math.log(b)));
-
-  if (remainder === 0) return [0];
-
-  for (let i = 0; remainder; remainder -= result[result.length - i - 1] * b ** i++) {
-    result[result.length - i - 1] = (remainder / b ** i % b)
-  }
-
-  return result;
+  // Convert digits in base a to base 10 then convert that number to base b.
+  return convertToBase(digits.reduce((num, digit) => digit + num * a), b);
 };
