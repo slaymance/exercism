ec666d1c83a596b2adb88229f4c5f366e33a25ef
Shane Laymance <shane.laymance@gmail.com>
2020-12-13 12:26:46 -0800

    Refactor to one horrific line

--- javascript/minesweeper/minesweeper.js
+++ javascript/minesweeper/minesweeper.js
@@ -16,16 +16,7 @@ const ADJACENTS = [
   [1, 1]
 ];
 
-export const annotate = input => input
-  .reduce((result, [...chars], row) => chars.reduce((result, char, col) => {
-    if (char === MINE) {
-      ADJACENTS.forEach(([rowAdj, colAdj]) => {
-        const [numRow, numCol] = [row + rowAdj, col + colAdj];
-        if (![MINE, undefined].includes(result[numRow]?.[numCol]))
-          result[numRow][numCol] = ~~result[numRow][numCol] + 1;
-      });
-    }
-
-    return result;
-  }, result), input.map(row => [...row]))
-  .map(row => row.join(''));
+/**
+ * I was so preoccupied with whether or not I could do it in one line, I didn't stop to think if I should.
+ */
+export const annotate = input => input.reduce((result, [...chars], row) => chars.reduce((result, char, col) => (char === MINE && ADJACENTS.map(([rowAdj, colAdj]) => [row + rowAdj, col + colAdj]).forEach(([row, col]) => ![MINE, undefined].includes(result[row]?.[col]) && ~~result[row][col]++)) || result, result), input.map(row => [...row])).map(row => row.join(''));
