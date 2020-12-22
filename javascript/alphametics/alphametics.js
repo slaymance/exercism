a08eb23625f0f2a70eba3e13d962dd7876c52a42
Shane Laymance <shane.laymance@gmail.com>
2020-12-21 18:52:46 -0800

    Optimize the solution using some math

--- javascript/alphametics/alphametics.js
+++ javascript/alphametics/alphametics.js
@@ -3,52 +3,40 @@
 // convenience to get you started writing code faster.
 //
 
-// Helper functions for clarity
-const isNumber = num => typeof num === 'number' && isFinite(num);
-const last = array => array[array.length - 1];
-
+const FULCRUM = ' == ';
 const DIGITS = [...Array(10).keys()];
 
-const evalPermuation = terms => isNumber(+last(terms)) &&
-  +last(terms) === terms.slice(0, -1).reduce((sum, term) => sum + +term, 0);
+const evalPermutation = (letterCounts, digits) => letterCounts
+  .reduce((sum, [, count], i) => sum + count * digits[i], 0) === 0
+  ? Object.fromEntries(letterCounts.map(([letter], i) => [letter, digits[i]]))
+  : null;
 
-const tryPermutations = (terms, digits = DIGITS, mapping = {}) => {
-  const letter = terms.flatMap(term => term.match(/[A-Z]/i) ?? [])[0];
+const tryPermutations = (terms, letterCounts, digits = []) => {
+  if (digits.length === letterCounts.length) return evalPermutation(letterCounts, digits);
 
-  if (!letter) return evalPermuation(terms) ? mapping : null;
+  for (const testDigit of DIGITS) {
+    if ([
+      digits.includes(testDigit),
+      testDigit === 0 && terms.some(term => term[0] === letterCounts[digits.length][0])
+    ].some(assertion => assertion)) continue;
 
-  for (const testDigit of digits) {
-    if (testDigit === 0 && terms.some(term => term[0] === letter)) continue; // Skip testing 0 if leading
+    const result = tryPermutations(terms, letterCounts, [...digits, testDigit]);
 
-    const permutationResult = tryPermutations(
-      terms.map(term => term.replace(new RegExp(letter, 'g'), testDigit)),
-      digits.filter(digit => digit !== testDigit),
-      { ...mapping, [letter]: testDigit }
-    );
-
-    if (permutationResult || testDigit === last(digits)) return permutationResult;
+    if (result) return result;
   }
+
+  return null;
 };
 
-export const solve = equation => tryPermutations(equation.match(/\w+/g));
+const countLetters = expressions => Object.entries(expressions
+  .reduce((counts, exp, right) => exp.reduce((counts, term) => [...term].reduceRight((counts, letter, i, { length }) =>
+    ({ ...counts, [letter]: ~~counts[letter] + 10 ** (length - 1 - i) * (right ? -1 : 1) }),
+    counts), counts), {}));
 
-/**
- * I went with a brute-force approach which isn't very efficient. I toyed with the idea of solving this
- * using a more deductive approach, but quickly realized it would be pretty complicated to code. Here's
- * some of the notes I took on rules that would be applied:
- *
- * General:
- *          left term = ((right term - sum of other left terms - carry) % 10 + 10) % 10
- *          right term = (sum of left terms + carry) % 10
- *
- * Carry Rules:
- *   - Column (A) has length of 1:
- *          column has carry
- *          A = 1
- *   - Column (AB) has length of 2:
- *          column has carry
- *          A = B % 10 - carry
- *          B = (A + carry) % 10
- *
- * Let me know if you attempt a non-brute force approach. I'd be interested to see it!
- */
+export const solve = equation => {
+  const expressions = equation
+    .split(FULCRUM)
+    .map(expression => expression.match(/\w+/g))
+
+  return tryPermutations(expressions.flat(), countLetters(expressions));
+};
