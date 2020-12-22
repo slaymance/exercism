e5f7164446088c66de958aff8940fac38fe5bff9
Shane Laymance <shane.laymance@gmail.com>
2020-12-21 16:56:42 -0800

    Move comments to bottom of file

--- javascript/alphametics/alphametics.js
+++ javascript/alphametics/alphametics.js
@@ -9,26 +9,6 @@ const last = array => array[array.length - 1];
 
 const DIGITS = [...Array(10).keys()];
 
-/**
- * I went with a brute-force approach which isn't very efficient. I toyed with the idea of solving this
- * using more deductive approach, but quickly realized it would be pretty complicated to code. Here's
- * some of the notes I took on rules that would be applied:
- * 
- * General: 
- *          left term = ((right term - sum of other left terms - carry) % 10 + 10) % 10
- *          right term = (sum of left terms + carry) % 10
- * 
- * Carry Rules:
- *   - Column (A) has length of 1: 
- *          column has carry
- *          A = 1
- *   - Column (AB) has length of 2:
- *          column has carry
- *          A = B % 10 - carry
- *          B = (A + carry) % 10
- * 
- * Let me know if you attempt a non-brute force approach. I'd be interested to see it!
- */
 const evalPermuation = terms => isNumber(+last(terms)) &&
   +last(terms) === terms.slice(0, -1).reduce((sum, term) => sum + +term, 0);
 
@@ -51,3 +31,24 @@ const tryPermutations = (terms, digits = DIGITS, mapping = {}) => {
 };
 
 export const solve = equation => tryPermutations(equation.match(/\w+/g));
+
+/**
+ * I went with a brute-force approach which isn't very efficient. I toyed with the idea of solving this
+ * using a more deductive approach, but quickly realized it would be pretty complicated to code. Here's
+ * some of the notes I took on rules that would be applied:
+ *
+ * General:
+ *          left term = ((right term - sum of other left terms - carry) % 10 + 10) % 10
+ *          right term = (sum of left terms + carry) % 10
+ *
+ * Carry Rules:
+ *   - Column (A) has length of 1:
+ *          column has carry
+ *          A = 1
+ *   - Column (AB) has length of 2:
+ *          column has carry
+ *          A = B % 10 - carry
+ *          B = (A + carry) % 10
+ *
+ * Let me know if you attempt a non-brute force approach. I'd be interested to see it!
+ */
