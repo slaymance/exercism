85129035dcf7824127ac78f1eade446be0e0125f
Shane Laymance <shane.laymance@gmail.com>
2020-12-23 00:59:55 -0800

    Download javascript/resistor-color solution

---
+++ javascript/resistor-color/resistor-color.js
@@ -0,0 +1,19 @@
+//
+// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
+];
+
+export const colorCode = color => COLORS.indexOf(color);
