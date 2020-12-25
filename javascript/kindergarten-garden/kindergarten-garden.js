3204455a81f02661e44199f2788fee57f140b99c
Shane Laymance <shane.laymance@gmail.com>
2020-12-24 16:39:03 -0800

    Refactor garden to use Proxy

--- javascript/kindergarten-garden/kindergarten-garden.js
+++ javascript/kindergarten-garden/kindergarten-garden.js
@@ -5,51 +5,42 @@
  *
  */
 
-const STUDENTS = [
-  'Alice',
-  'Bob',
-  'Charlie',
-  'David',
-  'Eve',
-  'Fred',
-  'Ginny',
-  'Harriet',
-  'Ileana',
-  'Joseph',
-  'Kincaid',
-  'Larry'
-];
-
-const PLANTS = {
-  C: 'clover',
-  G: 'grass',
-  R: 'radishes',
-  V: 'violets'
-};
-
-/**
- * I just wasn't feeling a class, you know?
- */
-const studentPlants = (cups, students) => {
-  const rows = cups.split('\n');
-  const sortedStudents = students?.slice().sort() ?? STUDENTS;
-  return student => {
-    const cupNum = sortedStudents.findIndex(testStudent => new RegExp(student, 'i').test(testStudent)) * 2;
-    return rows.flatMap(row => [...row.slice(cupNum, cupNum + 2)]).map(cup => PLANTS[cup]);
-  };
-};
-
-/**
- * The below code is only used to make the tests pass.
- */
-
 /* eslint-disable no-constructor-return */
+
+/**
+ * My first Proxy! It's useful here since we want to create a property for each student
+ * on the class. Think of the Proxy as a "catch-all" getter.
+ */
 export class Garden {
+  static STUDENTS = [
+    'Alice',
+    'Bob',
+    'Charlie',
+    'David',
+    'Eve',
+    'Fred',
+    'Ginny',
+    'Harriet',
+    'Ileana',
+    'Joseph',
+    'Kincaid',
+    'Larry'
+  ];
+  static PLANTS = {
+    C: 'clover',
+    G: 'grass',
+    R: 'radishes',
+    V: 'violets'
+  };
+
   constructor(cups, students) {
+    const rows = cups.split('\n');
+    const sortedStudents = students?.slice().sort() ?? Garden.STUDENTS;
     return new Proxy(this, {
       get(_, student) {
-        return studentPlants(cups, students)(student);
+        const cupNum = sortedStudents.findIndex(testStudent => new RegExp(student, 'i').test(testStudent)) * 2;
+        return rows.flatMap(row => [...row.slice(cupNum, cupNum + 2)]).map(cup => Garden.PLANTS[cup]);
       }
-    })
+    });
   }
 }
