7a5ce52bb9974836d7303e32f864d84b2cdd790c
Shane Laymance <shane.laymance@gmail.com>
2020-12-24 20:29:28 -0800

    Initialize typescript solutions

---
+++ typescript/hello-world/hello-world.ts
@@ -0,0 +1,17 @@
+/**
+ * Check out all my TypeScript Exercism solutions at:
+ *
+ * github.com/slaymance/exercism/tree/master/typescript
+ *
+ */
+
+const hello = (): string => 'Hello, World!';
+
+/**
+* The below code is only used to make the tests pass.
+*/
+class HelloWorld {
+  static hello(): string { return hello() }
+}
+
+export default HelloWorld
