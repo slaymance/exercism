5bfc8f8852183256bae78ef5fa491202107e2a84
Shane Laymance <shane.laymance@gmail.com>
2021-01-15 09:18:52 -0800

    Semicolon

--- javascript/run-length-encoding/run-length-encoding.js
+++ javascript/run-length-encoding/run-length-encoding.js
@@ -5,6 +5,6 @@
  *
  */
 
-export const encode = string => string.replace(/(.)\1+/g, ({ length }, char) => length + char)
+export const encode = string => string.replace(/(.)\1+/g, ({ length }, char) => length + char);
 
 export const decode = string => string.replace(/(\d+)(.)/g, (_, length, char) => char.repeat(length));
